e6e6f417a35ea0374158969eb083e7d03044035e
MLZ94 <zugutoroberto@gmail.com>
2021-05-18 23:49:10 +0200

    bringup p404

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 -u https://github.com/NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/MLZ94/local_manifest -b master .repo/local_manifests
+git clone https://github.com/MLZ94/local_manifest -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch nezuko_X00QD-userdebug
-mka bacon
+mka bacon -j8
 
 # upload rom
-rclone copy out/target/product/X00QD/NEZUKO*.zip cirrus:X00QD -P
+rclone copy out/target/product/X00QD/*2021*.zip cirrus:X00QD -P
