41c414db3e563bae60eabb7fb0df8135c172c2b4
MLZ94 <zugutoroberto@gmail.com>
2021-05-19 02:51:36 +0200

    lunch

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch nezuko_X00QD-userdebug
+lunch p404_X00QD-eng
 mka bacon -j9
 
 # upload rom
