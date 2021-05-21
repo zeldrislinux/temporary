02b98f95e6beeda70cf1722161fc75ef7687ceff
MLZ94 <zugutoroberto@gmail.com>
2021-05-21 06:59:50 +0200

    trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/MLZ94/local_manifest -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 source build/envsetup.sh
 lunch p404_X00QD-userdebug
