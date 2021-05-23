9bbccbb6dba6cd55f5a5afce2ccccb2a6ea44dcf
MLZ94 <zugutoroberto@gmail.com>
2021-05-23 17:36:21 +0200

    trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/MLZ94/local_manifest -b p404 .repo/local_manifests
+repo init -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/MLZ94/local_manifest -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 source build/envsetup.sh
-lunch p404_X00QD-userdebug
-make p404
+lunch aosp_X00QD-userdebug
+make bacon
 
 # upload rom
 rclone copy out/target/product/X00QD/*2021*.zip cirrus:X00QD -P
