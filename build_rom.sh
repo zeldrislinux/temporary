65907655a205f6b42703928727d152e0865f1ca5
boedhack99 <boedhack@gmail.com>
2021-05-27 15:22:07 +0700

    LineageOS: Now using script from main branch [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/boedhack/local_manifest.git --depth 1 -b havoc .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/boedhack/local_manifest.git --depth 1 -b los-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch havoc_mojito-userdebug
+lunch lineage_mojito-userdebug
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-brunch mojito
+mka bacon
 
 # upload rom
 rclone copy out/target/product/mojito/*.zip cirrus:mojito -P
