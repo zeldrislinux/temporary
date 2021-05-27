05ffc2e233bcc4b386c44d5880a002078908dedf
susandahal <susandahal53@gmail.com>
2021-05-27 15:14:29 +0000

    lavender: Build styx

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u  https://github.com/StyxProject/manifest -b R -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/susandahal/local_manifest --depth 1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch styx_lavender-user
+m styx-ota
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/vayu/styx*-UNOFFICIAL*.zip cirrus:lavender -P
