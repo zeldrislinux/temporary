42a252441588e043f4cf4cdc633e98379a506d5e
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2021-05-27 13:17:58 +0530

    begonia-CipherOS-HyperNotAryanX97

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/HyperNotAryanX97/Begonia -b backup .repo/local_manifests
+repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/HyperNotAryanX97/Begonia -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_begonia-userdebug
+lunch lineage_begonia-userdebug
 mka bacon
 
 # upload rom
