645d60b7997baafa606062400c98a518325ea21c
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-28 12:11:55 +0530

    Try with path
    Try fixing obb access

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,9 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Lineage-FE/manifest.git
 git clone https://github.com/baibhab34/local_manifest --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# patch
+cd frameworks/native && curl -O https://github.com/phhusson/platform_frameworks_native/commit/cc94e422c0a8b2680e7f9cfc391b2b03a56da765.patch && patch -p1 < *.patch && cd ../..
+
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX1805-userdebug
