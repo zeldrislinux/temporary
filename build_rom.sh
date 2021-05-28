6fce614b15712ba27ab5c2d3c3d0e45484895ace
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-05-29 01:24:16 +0530

    Last build for a while 😔

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync ro
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Shazu-xD/local_manifest.git --depth 1 -b Msm .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
