009852e198bce5cbedc288b62079b98926175f25
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2021-05-29 14:45:26 +0530

    Up-Streamed from main branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/HyperNotAryanX97/Begonia -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -8,5 +8,5 @@ source build/envsetup.sh
 lunch lineage_begonia-userdebug
 mka bacon
 
-# upload rom
-rclone copy out/target/product/begonia/CipherOS*.zip cirrus:begonia -P
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
