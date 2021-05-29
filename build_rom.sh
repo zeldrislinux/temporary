6a315e717d3196e9e467a616824b26cf8ec49bce
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-29 20:20:59 +0530

    bring up wave

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#Sync ROM
+repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
 
-# build rom
+git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b wave-os .repo/local_manifests
+
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# Build ROM
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch wave_mido-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+mka clean
+mka bacon -j$(nproc --all)
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# Upload build
+rclone copy out/target/product/mido/*.zip cirrus:mido -P
