26b114fae9841d4f584a5b67df8e14fd293aa8fe
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-30 18:52:06 +0530

    finally fixed

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 #Sync ROM
-repo init --depth=1 -u https://github.com/yashlearnpython/manifest.git -b r -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b wave-os .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Build ROM
-source build/envsetup.sh
+. build/envsetup.sh
 lunch wave_mido-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-mka clean
 mka bacon -j$(nproc --all)
 
 # Upload build
