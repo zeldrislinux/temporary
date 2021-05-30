271d4e17c3204301c10c4ef1613ea4ac481a3061
rahul9999xda <rahul9999xda@gmail.com>
2021-05-30 13:46:24 +0000

    Add one more patch
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 cd device/qcom/common && curl -L -o mypatch.patch https://github.com/AOSPA/android_device_qcom_common/commit/832de59190dd996820281e78dd4afd55fe13dd9c.patch && patch -p1 < mypatch.patch && cd -
 cd vendor/pa && curl -L -o mypatch.patch https://github.com/AOSPA/android_vendor_pa/commit/ec27f18b4a374f7041e1693058e205e0fcfbe734.patch && patch -p1 < mypatch.patch && cd -
 cd vendor/pa && curl -L -o mypatch.patch https://github.com/AOSPA/android_vendor_pa/commit/f0bfe716960ee8498d6a9442d734615909163d6b.patch && patch -p1 < mypatch.patch && cd -
+cd device/qcom/kernelscripts && curl -L -o mypatch.patch https://github.com/AOSPA/android_kernel_build/commit/0dfbc9cdf2f6ca505604859ec275ba4a0b81f038.patch && patch -p1 < mypatch.patch && cd -
 
 # build rom
 #build/envsetup.sh
