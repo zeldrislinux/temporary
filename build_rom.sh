8760d553a9b5f2369d09c054b140b7519b912630
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-06-01 14:27:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Stealth1226/local_manifest --depth 1 -b raft .repo/local_manifests
+git clone https://github.com/galanteria01/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-brunch rs988
+brunch violet
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/rs988/*OFFICIAL*.zip cirrus:rs988 -P
+rclone copy out/target/product/violet/*OFFICIAL*.zip cirrus:violet -P
