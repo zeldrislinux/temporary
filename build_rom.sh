8fbed8d24a5642d55f7b404125f9b4f110e7009f
rahul9999xda <rahul9999xda@gmail.com>
2021-06-01 13:26:05 +0000

    Trigger AOSPA
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/derp-sdm660-common/Local-Manifests.git --depth 1 -b aospa .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #patches
-cd device/qcom/common && curl -L -o mypatch.patch https://github.com/AOSPA/android_device_qcom_common/commit/832de59190dd996820281e78dd4afd55fe13dd9c.patch && patch -p1 < mypatch.patch && cd -
+cd device/qcom/common && curl -L -o mypatch.patch https://github.com/AOSPA/android_device_qcom_common/commit/b42838e08f2e21cc1ce78961a8bca3a7e40d914e.patch && patch -p1 < mypatch.patch && cd -
 cd vendor/pa && curl -L -o mypatch.patch https://github.com/AOSPA/android_vendor_pa/commit/ec27f18b4a374f7041e1693058e205e0fcfbe734.patch && patch -p1 < mypatch.patch && cd -
 cd vendor/pa && curl -L -o mypatch.patch https://github.com/AOSPA/android_vendor_pa/commit/f0bfe716960ee8498d6a9442d734615909163d6b.patch && patch -p1 < mypatch.patch && cd -
 cd device/qcom/kernelscripts && curl -L -o mypatch.patch https://github.com/AOSPA/android_kernel_build/commit/0dfbc9cdf2f6ca505604859ec275ba4a0b81f038.patch && patch -p1 < mypatch.patch && cd -
