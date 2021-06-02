acca4c226c50e6a6a21cc1ce8c05f18e997a5204
Sarthak <sarthakroy2002@gmail.com>
2021-06-02 11:44:54 +0530

    Fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Realme-MT6768-Developments/local_manifest_30A.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Realme-MT6768-Developments/local_manifest_30A.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # patch
