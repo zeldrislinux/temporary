ec337170c3fb06f9742d39804c074c55a37bb111
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-06-04 09:22:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 brunch violet
+
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/violet/*OFFICIAL*.zip cirrus:violet -P
