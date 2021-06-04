c6c2795d85fe7e44210f24573e5650029ab209be
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-06-04 09:57:24 +0530

    Get rclone from main

--- build_rom.sh
+++ build_rom.sh
@@ -13,4 +13,4 @@ lunch lineage_RMX1805-userdebug
 mka bacon
 
 # upload rom
-rclone copy out/target/product/RMX1805/LineageFE*RMX1805*.zip cirrus:RMX1805 -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
