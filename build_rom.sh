63f76097da931be6f0383549969b7d8a4468acb9
P-Salik <peersalik16@gmail.com>
2021-06-05 01:32:50 +0530

    Octavi RMX1941

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,17 @@
 # sync rom
-repo init --depth=1 -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 
-git clone https://github.com/P-Salik/local_manifest --depth=1 -b CorvusOS .repo/local_manifests
+git clone https://github.com/P-Salik/local_manifest --depth=1 -b Octavi .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #patches
 
-cd external/selinux
-curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
-patch -p1 < *.patch
-cd ../..
+#cd external/selinux
+#curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+#patch -p1 < *.patch
+#cd ../..
+
 cd frameworks/opt/net/ims
 curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
 patch -p1 < *.patch
@@ -23,8 +24,8 @@ cd ../../../..
 
 # build
 . build/envsetup.sh
-lunch corvus_RMX1941-userdebug
-make corvus -j$(nproc --all)
+lunch octavi_RMX1941-userdebug
+brunch RMX1941
 
 # upload build
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
