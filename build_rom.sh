d266fe92c1b5dd8edd04b35206f9787a3e97c18e
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-06-04 09:54:31 +0530

    Add a patch

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # patch
 cd frameworks/native && curl -O https://github.com/phhusson/platform_frameworks_native/commit/cc94e422c0a8b2680e7f9cfc391b2b03a56da765.patch && patch -p1 < *.patch && cd ../..
+cd external/selinux && curl -O https://github.com/phhusson/platform_external_selinux/commit/38d614ec61d610459a7f8e3a243a3dab7a20d356.patch && patch -p1 < *.patch && cd ../..
 
 # build rom
 source build/envsetup.sh
@@ -13,4 +14,3 @@ mka bacon
 
 # upload rom
 rclone copy out/target/product/RMX1805/LineageFE*RMX1805*.zip cirrus:RMX1805 -P
-
