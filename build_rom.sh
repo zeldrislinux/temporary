4ef426eb613f50fc77569b1c5d27b69b4198e0f0
boedhack99 <boedhack@gmail.com>
2021-06-05 08:45:07 +0700

    Trigger: userdebug -> user

--- build_rom.sh
+++ build_rom.sh
@@ -5,13 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch havoc_mojito-user
-export SKIP_API_CHECKS=true
-export SKIP_ABI_CHECKS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
-brunch mojito
+brunch havoc_mojito-user
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
