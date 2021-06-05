3a1d2d9aa263dc0e054323af493fe299f7fa2316
susandahal <susandahal53@gmail.com>
2021-06-05 08:00:47 +0000

    build.sh: Update according to main branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u  https://github.com/StyxProject/manifest -b R -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/susandahal/local_manifest --depth 1 -b styx .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest -b R -g default,-device,-mips,-darwin,-notdefault
+https://github.com/susandahal/local_manifest --depth 1 -b styx .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch styx_lavender-user
+export TZ=Asia/Dhaka #put before last build command
 m styx-ota
 
-# upload rom
-rclone copy out/target/product/lavender/styx*-UNOFFICIAL*.zip cirrus:lavender -P
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
