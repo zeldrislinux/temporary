f766bec9b2783c0e6819395bea95a719fadec183
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-06-08 17:23:43 +0530

    Was manifest error

--- build_rom.sh
+++ build_rom.sh
@@ -4,11 +4,8 @@ git clone https://github.com/flashokiller/mainfest_personal --depth 1 -b pa-ysl 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #.
 #ild rom
-source build/envsetup.sh
-lunch pa_ysl-userdebug
 export TZ=Asia/kolkata #put before last build command
-make bacon
-
+./buildrom.sh ysl
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
