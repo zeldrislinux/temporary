70a7b3217b04c02e0e6b76a66e7f8e880bd65ab6
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-06-08 16:16:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,8 +4,8 @@ git clone https://github.com/flashokiller/mainfest_personal --depth 1 -b pa-ysl 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #.
 # build rom
-#source build/envsetup.sh
-#lunch pa_ysl-userdebug
+source build/envsetup.sh
+lunch pa_ysl-userdebug
 export TZ=Asia/kolkata #put before last build command
 make bacon
 
