45e050457bfc7c718019756dbca1225cb076bcfc
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-09 20:12:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,6 @@ git clone https://github.com/Yasundram/local_manifest --depth 1 -b main .repo/lo
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
-
 # build rom
 . build/envsetup.sh
 lunch cherish_RMX1941-userdebug
@@ -16,9 +14,5 @@ export CHERISH_NONGAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka cherish
 
- 
-
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
- 
- 
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P 
