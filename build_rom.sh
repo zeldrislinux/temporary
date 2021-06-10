870021155f44ae0152c469702ced3f09e613ecae
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-06-10 11:20:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b rub
 git clone https://github.com/flashokiller/mainfest_personal --depth 1 -b pa-ysl .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#build rom
+#build rom.
 #. build/envsetup.sh
 #lunch pa_ysl-userdebug
 export TZ=Asia/kolkata #put before last build command
