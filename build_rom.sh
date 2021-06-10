8c88ce253f90589935c4f4c2d4a8ea8a454219c2
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-10 17:32:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh 
-lunch styx_RMX1941-userdebug 
+lunch styx_RMX1941-userdebug  
 m styx-ota
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
