fa417ce67bccc049adfdd73760b424416b01d226
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-06-10 19:28:17 +0530

    adding selinux patch

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,11 @@ git clone https://github.com/Yasundram/local_manifest --depth 1 -b main .repo/lo
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+cd external/selinux
+curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+patch -p1 < *.patch
+cd ../..
+
 # build rom
 . build/envsetup.sh
 export SKIP_ABI_CHECKS=true
