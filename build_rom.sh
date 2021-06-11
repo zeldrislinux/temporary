10aac00e5d0919ec3a9929cb2b22cb3a8277faad
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-06-11 16:09:35 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export SKIP_ABI_CHECKS=true
 export TZ=Asia/Kolkata #put before last build command
 brunch RMX1801
 
