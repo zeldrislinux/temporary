92956ec8646bf12087a4452eafc080a9f521cba5
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-12 14:53:46 +0530

    LineageOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_RMX1941-userdebug
+lunch lineage_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
