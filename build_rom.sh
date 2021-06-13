5575b19343480c8c4501757e74bcb80aec325a14
Hadad <repo-sync@outlook.com>
2021-06-13 17:22:44 +0700

    [skip ci] temporary: add back commit d31e1f84a69530d06180e3e6c1cb0246eb2c9c51 of sync sources.
    Signed-off-by: Hadad <repo-sync@outlook.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://gitlab.com/hdrjt/local_manifest.git --depth=1 -b komodo .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
