4472b41fd73b8084365186df501a4aeedd1fd794
P-Salik <peersalik16@gmail.com>
2021-06-14 23:24:05 +0530

    Lineage RMX1941

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/P-Salik/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_RMX1941-userdebug
+lunch lineage_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
 
