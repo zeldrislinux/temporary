ada2bd85e659611e5a96b061867717d01df49a3c
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-06-14 14:42:39 +0530

    Remove all patches

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,6 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Lineage-FE/manifest.git
 git clone https://github.com/baibhab34/local_manifest --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# patch
-cd external/selinux && curl -O https://github.com/phhusson/platform_external_selinux/commit/38d614ec61d610459a7f8e3a243a3dab7a20d356.patch && patch -p1 < *.patch && cd ../..
-
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX1805-userdebug
