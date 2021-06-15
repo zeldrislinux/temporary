6cc9b32db79dfea8a203482f2afb7174a30ba0af
JamieHoSzeYui <henloboii@protonmail.ch>
2021-06-15 09:23:03 +0800

    build-rom: drop existing local-manifests first
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
- git clone https://github.com/exthm-miatoll/manifest --depth 1 -b master .repo/local_manifests
+rm -rf .repo/local_manifests
+git clone https://github.com/exthm-miatoll/manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
