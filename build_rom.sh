a74cc0a6a94f09372fc0342459ff380c420ce38e
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-06-15 20:06:46 +0530

    add depth and do userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yashlearnpython/local_manifest.git -b pa .repo/local_manifests
+git clone https://github.com/yashlearnpython/local_manifest.git --depth 1 -b pa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 export TZ=Asia/Kolkata
-./rom-build.sh mido
+./rom-build.sh mido -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
