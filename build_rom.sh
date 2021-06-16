8462fe74b379c994e475e93fbd1410b2e19199b8
P-Salik <peersalik16@gmail.com>
2021-06-17 01:05:36 +0530

    Legion:RMX1941

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/P-Salik/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/P-Salik/local_manifest --depth 1 -b LegionOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+cd frameworks/opt/net/ims
+curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
+patch -p1 < *.patch
+cd ../../../..
+
 # build rom
-source build/envsetup.sh
-lunch lineage_RMX1941-userdebug
+. build/envsetup.sh
+lunch legion_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon -j$(nproc --all)
+make legion -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
