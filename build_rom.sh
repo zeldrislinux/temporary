0a19c342bf5efe4a06fbdcab0cda545ea2b0025d
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-06-16 20:51:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch cherish_RMX1801-userdebug
 export TZ=Asia/Kolkata 
-mka bacon -j$(nproc --all)
+mka bacon
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
