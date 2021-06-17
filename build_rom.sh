1e5d95f34890028f7673d35148e3203b82575c9b
P-Salik <peersalik16@gmail.com>
2021-06-17 10:09:36 +0530

    RevengeOS: RMX1941

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/P-Salik/local_manifest --depth 1 -b LegionOS .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RevengeOS/android_manifest -b r11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/P-Salik/local_manifest --depth=1 -b RevengeOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch legion_RMX1941-userdebug
+source build/envsetup.sh
+lunch revengeos_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
-make legion -j$(nproc --all)
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
