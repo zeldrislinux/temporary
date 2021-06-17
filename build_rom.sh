e872519a623f88a239570654f83c872aeb2e05e8
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-06-17 12:05:31 +0530

    try to fix ril

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 #build rom
 source build/envsetup.sh
-lunch p404_mido-eng
+lunch p404_mido-userdebug
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
