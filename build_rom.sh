9c54d9e7f3e986c28123ef52e785dc6922d73b49
MLZ94 <zugutoroberto@gmail.com>
2021-06-17 23:49:02 +0200

    trigger

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/MLZ94/local_manifest -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
- 
+
 # build rom
 source build/envsetup.sh
 lunch aosp_X00QD-userdebug
