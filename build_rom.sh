e67c241f84d807a546fd2d4ec581cf348bf36ac3
B0ND <mhashimkp@gmail.com>
2021-06-17 23:45:08 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/malluboy/local_manifests --depth 1 -b aosp .repo/local_manifests
+git clone https://github.com/malluboy/local_manifests --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
