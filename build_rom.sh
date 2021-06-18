318bb8536f25c12a2e71c3df18c16c592d46dfb2
boedhack99 <boedhack@gmail.com>
2021-06-18 19:06:04 +0700

    Trigger: lineage vanilla build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/boedhack/local_manifest --depth=1 -b 11 .repo/local_manifests
+git clone https://github.com/boedhack/local_manifest.git --depth=1 -b 11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch lineage_mojito-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
