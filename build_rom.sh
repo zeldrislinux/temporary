587e76058211f5ece5e2672aa461a6428719499b
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-19 23:41:37 +0700

    last

--- build_rom.sh
+++ build_rom.sh
@@ -6,11 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch nad_RMX3171-userdebug
-export SKIP_API_CHECKS=true
-export SKIP_ABI_CHECKS=true
 export TZ=Asia/Jakarta
 mka nad
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
