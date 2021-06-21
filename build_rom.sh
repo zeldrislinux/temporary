9d003edbd8715d25d8977f1f7a5c2a4999afbff1
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-21 15:03:50 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b Styx .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/TenX-OS/manifest_TenX.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b TenX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh 
-lunch styx_RMX1941-userdebug  
-m styx-ota
+lunch aosp_RMX1941-userdebug  
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
