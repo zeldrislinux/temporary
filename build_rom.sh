110c24ea88a106bb8c49e9d6d3dd22cd7d11e520
Rehan Singh <rehandagur@gmail.com>
2021-06-21 11:46:07 +0530

    Trigger New Build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
-brunch merlin
 brunch harpia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
