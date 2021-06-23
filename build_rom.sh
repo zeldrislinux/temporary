950def1d15b842aaab3eee9eb00855b033e339a1
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-23 13:47:35 +0530

    Changed patches

--- build_rom.sh
+++ build_rom.sh
@@ -5,17 +5,6 @@ git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b dot .repo/
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#patches
-cd frameworks/base
-curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
-patch -p1 < *.patch
-cd ../..
-
-cd frameworks/opt/net/ims
-curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
-patch -p1 < *.patch
-cd ../../../..
-
 # build
 source build/envsetup.sh
 lunch dot_RMX1941-userdebug
