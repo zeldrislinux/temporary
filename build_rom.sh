d5f3151f0124d28f307a471555b2ae11c6cb08ac
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-06-24 15:05:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync romm
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/flashokiller/mainfest_personal --depth 1 -b pa-ysl .repo/local_manifests
+git clone https://github.com/flashokiller/manifest_pine --depth 1 -b pa-ysl .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build makr
 #. build/envsetup.sh
 #lunch pa_ysl-userdebug
 export TZ=Asia/kolkata #put before last build commandm..
-./rom-build.sh ysl
+./rom-build.sh pine
 # upload rom (if You don't need to upload multiple files, then you don't need to edit next line)........
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
