72846d85e9eb694b0cca609b985530ccd0e6f985
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-06-26 10:35:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g de
 git clone https://github.com/galanteria01/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom again
 source build/envsetup.sh
 lunch lighthouse_violet-user
 export TZ=Asia/Kolkata #put before last build command
