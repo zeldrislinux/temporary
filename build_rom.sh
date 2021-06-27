4ea2ffdab1f2b4dd818645c8191e22a463b20047
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-27 23:22:08 +0530

    Official

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
+export PEX_BUILD_TYPE=OFFICIAL
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
