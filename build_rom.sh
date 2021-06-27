754fe8f6a694963d0c79b89c8ed72875e67cb6b7
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-27 12:19:20 +0530

    Test #2

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX1941-userdebug
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/kolkata #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
