aa4fb23c3a778f41783d85835c33a97ce73880cd
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-28 10:46:44 +0530

    Trying ota

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,7 @@ lunch aosp_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
 export PEX_BUILD_TYPE=OFFICIAL
 mka bacon
+bash ./OTA/support/ota.sh RMX1941
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
