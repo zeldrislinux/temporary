c1c8ae435aeceec1a6e232c2eddde0341a6e7fe3
dustxyz <notreallydust@protonmail.com>
2021-06-27 20:39:00 -0400

    ysl: trigger build #6
    Signed-off-by: dustxyz <notreallydust@protonmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch potato_ysl-user && lunch potato_ysl-user
+lunch potato_ysl-userdebug
 export TZ=America/Bogota #put before last build command
 brunch ysl
 
