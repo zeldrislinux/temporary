e967ff7097e86e0582e78fa0cf2ab49d2928f33e
Adrian Lam <75461199+adrian-8901@users.noreply.github.com>
2021-06-30 07:16:51 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch fluid_umi-eng
-export ALLOW_MISSING_DEPENDENCIES=true
 export WITH_GAPPS=true
 export TZ=Asia/Hong_Kong
 mka bacon
