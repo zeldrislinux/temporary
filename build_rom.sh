1cf92a29d49cb8a26a65e716c87d4795e542c602
yash <73551736+yash3056@users.noreply.github.com>
2021-06-29 18:20:32 +0530

    sed life

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aaosp_vayu-userdebug
+lunch aosp_vayu-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
