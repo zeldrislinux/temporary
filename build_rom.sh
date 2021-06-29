55313db13ed00455ca8fa7ff73919fde73d5c778
yash <73551736+yash3056@users.noreply.github.com>
2021-06-29 14:27:43 +0530

    new kernel op

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yashlearnpython/local_manifest.git -b pe .repo/local_manifests
+git clone https://github.com/yash3056/local_manifest.git -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
