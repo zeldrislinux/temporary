cdc78674f3d6b02466d16b1a439560093fe116d7
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-06-29 17:56:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -q --no-repo-verify --depth=1 https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Shazu-xD/local_manifest.git --depth 1 -b corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
