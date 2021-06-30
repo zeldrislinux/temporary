db9a203265e8453e2b72519844fec33f86a73cd7
Sushmit <80753594+sushmit1@users.noreply.github.com>
2021-06-30 09:29:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch corvus_ysl-user
 export RAVEN_LAIR=Beta-official
-export USE_GAPPS=true
 make corvus
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
