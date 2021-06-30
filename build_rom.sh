1e40cedee76f3f016ec6ee537b08c88afcea713d
yash <73551736+yash3056@users.noreply.github.com>
2021-06-30 07:44:19 +0530

    new hals op

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b p404 .repo/local_manifests
+git clone https://github.com/yashlearnpython/local_manifest_mido.git --depth=1 -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build rom
