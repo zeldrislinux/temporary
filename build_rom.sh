7f98d95099f1ecb0d23ea4f07e5424996e4d28a1
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-07-01 21:33:43 +0600

    derpfest 11 rosy

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.gi
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch derp_rosy-userdebug
