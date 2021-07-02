148bda973add160c51f3f4d5bb5a6deac8c7e2bf
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-07-02 15:51:07 +0600

    fix fatal error & retry

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b rr .repo/loc
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
+
 # build rom
 source build/envsetup.sh
 lunch rr_rosy-userdebug
