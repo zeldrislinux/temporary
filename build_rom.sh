35ed0af0f775a086f37e24e9f40b625fd7d1dace
P-Salik <peersalik16@gmail.com>
2021-07-02 11:45:42 +0530

    Corvus

--- build_rom.sh
+++ build_rom.sh
@@ -5,19 +5,6 @@ git clone https://github.com/P-Salik/local_manifest --depth=1 -b CorvusOS .repo/
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#patches
-# [1] Boot
-cd external/selinux
-curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
-patch -p1 < *.patch
-cd ../..
-
-# [2] IMS
-cd frameworks/opt/net/ims
-curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
-patch -p1 < *.patch
-cd ../../../..
-
 # build
 . build/envsetup.sh
 lunch corvus_RMX1941-userdebug
