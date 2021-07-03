b9242a214122061dde0826ae7dd89a0c490e544f
Endi Priyatna <65471980+Dreadwyrm@users.noreply.github.com>
2021-07-03 17:24:23 +0700

    Trigger build aex

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch aosp_ginkgo-userdebug
 export TZ=Asia/Jakarta
 m aex -j$(nproc --all)
 
