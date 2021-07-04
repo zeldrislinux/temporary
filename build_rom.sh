4d5086c9cd36d2c2538de0767fec4afcb0d51610
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-07-04 09:50:39 +0600

    still giving fatal error ..so retry

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom-evo
 source build/envsetup.sh
 lunch evolution_rosy-userdebug
 export TZ=Asia/Dhaka #put before last build command
