f44d10d2a702e961761e7bb3ac753cfcfab28226
Anoos Ragh <anoosrockzzz@gmail.com>
2021-07-04 20:06:40 +0530

    Fixed overlay

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Wave-Project/manifest.g
 git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b wave .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build cmds
 source build/envsetup.sh
 export VANILLA_BUILD=true
 lunch wave_RMX1941-userdebug 
