bb1b1c63f722b18d0c061348fa4ce5f305e4bc45
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-07-04 09:46:44 +0600

    same rom running ..retry

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch evolution_rosy-userdebug
