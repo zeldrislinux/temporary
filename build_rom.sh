4c21c2fe77d45ceef666aa6eb8ee3028b08c11d7
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-07-06 15:57:58 +0600

    fix fatal error

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://gitlab.e.foundation/e/os/release
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch lineage_rosy-userdebug
