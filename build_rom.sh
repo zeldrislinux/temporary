5691684fb3011ef8285ea2ec35f8c92726e6a364
Endi Priyatna <65471980+Dreadwyrm@users.noreply.github.com>
2021-07-06 17:36:13 +0700

    ginkgo: Switch kernel & hals

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Dreadwyrm/local_manifests --depth 1 -b ginkgo-CipherOS .repo/local_manifests
+git clone https://github.com/Dreadwyrm/local_manifests --depth 1 -b ginkgo-Cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
