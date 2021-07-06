76c5efef438d4c4f4ff2f348fac3c2d57628cf08
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-07-06 15:54:56 +0600

    update buildrom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git@gitlab.e.foundation:e/os/releases.git -b v0.17.1-q -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://gitlab.e.foundation/e/os/releases.git -b v1-q -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
