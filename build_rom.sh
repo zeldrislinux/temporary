8b875297ce13905b2d9605be84741a0d54b3df18
yash <73551736+yash3056@users.noreply.github.com>
2021-07-06 23:19:37 +0530

    do userdebug build

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch wave_vayu-eng
-export TZ=Asia/Dhaka #put before last build command.
+lunch wave_vayu-userdebug
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
