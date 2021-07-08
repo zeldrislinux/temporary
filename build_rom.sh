11e8006ba5a601ed3c3dddbe23f280357f73a185
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-07-08 12:17:02 +0530

    july sp

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b test -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/Yasundram/local_manifest --depth 1 -b cherish .repo/local_manifests
 
@@ -9,7 +9,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export SKIP_ABI_CHECKS=true
 export CHERISH_NONGAPPS=true
-export CHERISH_MAINTAINER=Sundram  
+export CHERISH_MAINTAINER=Sundram™  
 export TZ=Asia/Dhaka #put before last build command
 brunch RMX1941
 
