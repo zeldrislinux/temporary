994baccefc3f9ab6596fbd2ab33e988a8ee3b50b
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-07-10 01:38:09 +0700

    build gapps

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+export WITH_GMS = true
 lunch cherish_lavender-userdebug
 export TZ=Asia/Ho_Chi_Minh 
 mka bacon -j$(nproc --all)
