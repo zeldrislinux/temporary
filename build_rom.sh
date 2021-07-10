b42ba933540125e148fcfbdb30b73566b5fca9bb
D4rkKnight21 <yusuff8621@gmail.com>
2021-07-10 17:48:51 +0700

    build cafex : try fix error on prebuilt clang

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CAF-Extended/manifest -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/D4rkKnight21/local_manifest --depth 1 -b cafex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/D4rkKnight21/local_manifest --depth 1 -b cafex .repo/local_manifests
 
 # build rom
 . build/envsetup.sh
 lunch cafex_platina-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
+export SKIP_API_CHECK=true
+export SKIP_ABI_CHECK=true
 export TZ=Asia/Jakarta
 m cafex -j$(nproc --all)
 
