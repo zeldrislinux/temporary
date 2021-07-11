3b942074d163647af31f96edb959d56ed831df08
rahul9999xda <rahul9999xda@gmail.com>
2021-07-11 18:14:37 +0000

    Fix neverallows and try user build
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch havoc_vayu-userdebug
+lunch havoc_vayu-user
 export TZ=Asia/Dhaka #put before last build command
-export SELINUX_IGNORE_NEVERALLOWS := true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
