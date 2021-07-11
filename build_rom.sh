8d812703f56768fba53cb39c1cd39c0e9974c5b0
boedhack <rudipratama070@gmail.com>
2021-07-11 07:53:45 +0700

    Trigger: Build: fix sepolicy denial and legacy for hals bootctl default

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/boedhack/local_manifest.git --depth=1 -b 11 .repo/l
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch lineage_mojito-userdebug
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
