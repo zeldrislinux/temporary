bfd50d7d7f516fde531e70b73d67a35e6645cbbc
neonmicky <63160537+neonmicky@users.noreply.github.com>
2021-07-14 04:35:37 +0530

    Kernel stuffs

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 
-# sync  rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/neonmicky/local_manifest.git --depth 1 -b xr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
