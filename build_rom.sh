6774d5d7b4f060afe98aaf1c5b17d467eaf740e5
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-07-17 21:22:16 +0530

    try fixing bt bug
    Test build

--- build_rom.sh
+++ build_rom.sh
@@ -8,5 +8,6 @@ source build/envsetup.sh
 lunch lineage_RMX1805-userdebug
 mka bacon
 
+
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
