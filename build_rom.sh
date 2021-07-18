e5e89cf30dadb8c2ae020effe80d6b5150f8583d
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-07-18 11:43:23 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX1941-userdebug
+lunch lineage_RMX2193-userdebug
 export TZ=Asia/kolkata #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 mka bacon
