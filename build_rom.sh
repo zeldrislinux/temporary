04e58716516f549596bc7fbf2f611ddcbed931b7
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-07-20 12:14:41 +0530

    Intial trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b sakura .repo/local_manifests
+git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX1941-userdebug
+lunch lineage_RMX2193-userdebug
 export TZ=Asia/kolkata #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 mka bacon
