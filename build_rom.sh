6916e870e7b4b698581778743a502da4742f122f
Wiley Lau <henloboii@protonmail.ch>
2021-07-22 10:56:49 +0800

    temporary: build_rom: switch back to userdebug
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch styx_cheeseburger-user
+lunch styx_cheeseburger-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m styx-ota
 
