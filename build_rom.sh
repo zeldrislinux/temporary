8aab4e3171792e35d23aa2415599297e265b817a
Wiley Lau <henloboii@protonmail.ch>
2021-07-22 13:19:18 +0800

    temporary: drop sed totally
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
-sed -i "|frameworks/base|d" /home/cirrus/roms/StyxProject/.repo/manifest.xml
-git clone https://github.com/JamieHoSzeYui/manifest --depth 1 -b styx .repo/local_manifests
+\git clone https://github.com/JamieHoSzeYui/manifest --depth 1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 
 # build rom
