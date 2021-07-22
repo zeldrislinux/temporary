96885dca8b3ae03877442e6cc595f9f4f2fc9482
Wiley Lau <henloboii@protonmail.ch>
2021-07-22 10:23:30 +0800

    temporary: this isn't derpfest
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/JamieHoSzeYui/manifest --depth 1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 
