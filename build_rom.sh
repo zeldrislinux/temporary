27c8adaf19b89ffbcf4b05024f4598e8b5b4b126
Wiley Lau <henloboii@protonmail.ch>
2021-07-22 12:32:12 +0800

    temporary: drop the use of sed
    * switch to cherrypicking instead.
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
-sed -i "|frameworks/base|d" /home/cirrus/roms/StyxProject/.repo/manifest.xml
 git clone https://github.com/JamieHoSzeYui/manifest --depth 1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+
+# apply required commits
+cd frameworks/base/
+git fetch https://github.com/JamieHoSzeYui/frameworks_base --depth=3 
+git cherry-pick 34b692197912697521dd5bd4f1fedc83f0ca3d96 &&git cherry-pick 32cc051c3d14f072503a524e9617130324a26f4c &&git cherry-pick a3e3616b3327f56cc01da1bc529e20b755d2c0bb
+cd ../../
 
 # build rom
 source build/envsetup.sh
