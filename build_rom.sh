ae61f4a2f0befc1dcc776072be8548c01660562e
Wiley Lau <henloboii@protonmail.ch>
2021-07-23 21:57:25 +0800

    temporary: fix

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch statix_cheeseburger-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export BUILD_BROKEN_OUTSIDE_INCLUDE_DIRS=true
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
