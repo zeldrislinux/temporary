53eefbc9afd52f1a9460566c1d1e7ee6f49c0d71
boedhack99 <boedhack@gmail.com>
2021-07-27 00:44:50 +0700

    Trigger: Try to fix separate building with modify partitions type

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/boedhack/local_manifest.git --depth 1 -b AEX .repo/local_manifests
+git clone https://github.com/boedhack99/local_manifest.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
