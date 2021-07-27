3c291602b94c3fabd5ab618732e0eb1b83557f95
Yusuf Abidin <yusuff8621@gmail.com>
2021-07-28 05:54:28 +0700

    Fix manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/DHAMPOT/dependencies.git --depth=1 -b styx .repo/local_manifests
+git clone https://github.com/DHAMPOT/local_manifest.git --depth=1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch styx_platina-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_API_CHECK=true
 export SKIP_ABI_CHECK=true
 export TZ=Asia/Jakarta
