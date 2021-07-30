fbb3c18b0003a66c5a84871a474a97c174f198c6
yash <73551736+yash3056@users.noreply.github.com>
2021-07-30 11:19:45 +0530

    add never allow tag

--- build_rom.sh
+++ build_rom.sh
@@ -7,9 +7,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vayu-user
+lunch aosp_vayu-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
