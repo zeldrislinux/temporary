d18ec705c1f1efc94c0ac36d2ad7a5e0320b8a91
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-01 20:05:48 +0530

    Initial Build with VOLTE nd stuff

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/sarthakroy2002/local_manifest.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2020-userdebug
+lunch potato_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
-mka bacon -j$(nproc --all)   
+brunch RMX2020  
 
 # upload  rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
