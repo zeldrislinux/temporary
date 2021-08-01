acd4624a597b953925bec5767819507b9ef825a1
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-08-01 20:10:19 +0530

    Fixed typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Yasundram/Local-Manifests.git --depth 1 -b 30a .repo/local_manifests
+git clone https://github.com/Yasundram/local_manifest --depth 1 -b 30a .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 source build/envsetup.sh
