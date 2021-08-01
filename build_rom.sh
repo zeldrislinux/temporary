b3bb9d95e2bfe5ef50e45c13c426f9136ac3eef4
Asif <49162687+nhAsif@users.noreply.github.com>
2021-08-01 08:55:17 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b legion .repo/local_manifests
+git clone https://github.com/nhAsif/device_qcom_sepolicy-legacy-um.git --depth 1 -b 11
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
