20cbec57cc6f02200fac248b8de0a68813d50bee
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-08-04 02:38:16 +0700

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Realme-G70-Series/local_manifest.git --depth 1 -b rmui2 .repo/local_manifests
+git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2020-userdebug
+lunch lineage_RMX1941-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
