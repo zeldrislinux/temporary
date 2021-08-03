af1eb72e3ee1b346a524692cb278d75b4aac5b3f
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-03 18:57:01 +0530

    CIPHER OSS BUILD INITIAL

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
+git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export TARGET_FACE_UNLOCK_SUPPORTED=true
+export CIPHER_GAPPS=true
+export TARGET_USES_BLUR=true
+export CIPHER_OFFICIAL=true
 mka bacon -j$(nproc --all) 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
