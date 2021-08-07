dc1404e13fa57d2be587c348c5b980ba80135b67
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-08-07 21:08:18 +0530

    Lighthouse on7xelte initial build trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Stealth1226/local_manifest --depth 1 -b raft .repo/local_manifests
+git clone https://github.com/Stealth1226/local_manifest --depth 1 -b raft-j7 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lighthouse_rs988-user
+lunch lighthouse_on7xelte-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 mka lighthouse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/rs988/*rs988*json cirrus:rs988 -P
