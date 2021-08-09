bb710b290d3961c31b1c024cde988a87a82b0a25
Harshhaareddy <79190327+harshhaareddy@users.noreply.github.com>
2021-08-09 21:10:23 +0530

    Fixed manifest

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch ssos_X00TD-userdebug
+export SKIP_API_CHECKS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
