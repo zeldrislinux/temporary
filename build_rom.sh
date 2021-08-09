667d03afc482f648b4fca68ea843251890386a73
Harshhaareddy <79190327+harshhaareddy@users.noreply.github.com>
2021-08-09 13:25:56 +0530

    Fixed duplicate paths and Hal's

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/harshhaareddy/local-manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/harshhaareddy/local-manifest.git --depth 1 -b ssos11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
