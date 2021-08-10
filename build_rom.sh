10cfa4da8c28780d950eafa0bdcb5ff738a28bf8
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-10 11:02:10 +0530

    INITIAL VOLTE BUILD

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last  build command
 export TARGET_FACE_UNLOCK_SUPPORTED=true
 export CIPHER_GAPPS=true
 export TARGET_USES_BLUR=true
