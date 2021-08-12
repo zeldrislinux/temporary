d868a77745607b611d1aabd0a4630839fd0e5b7a
Riot-X <abhiragh0@gmail.com>
2021-08-12 13:43:16 +0530

    fix bootloop

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch potato_olives-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 brunch olives
 
