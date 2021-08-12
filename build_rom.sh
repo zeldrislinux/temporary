f9cab7f8bd000c3339fca7b7fa4eb714408c14e4
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-08-12 20:24:55 +0530

    Fixing minor bugs

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b test -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/Yasundram/local_manifest --depth 1 -b cherish .repo/local_manifests
 
