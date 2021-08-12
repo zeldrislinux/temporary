fcc47422b4c6beedd60b2ff6e940f879aa4c36f5
tatannn <tatanstfu@gmail.com>
2021-08-12 09:07:54 +0700

    export SELINUX_IGNORE_NEVERALLOWS

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch bliss_ginkgo-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 blissify -v ginkgo
 
