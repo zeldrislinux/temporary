dd056fecd344a3485904be8bdc798ad924b3d1cc
Ayashaa <tatanstfu@gmail.com>
2021-08-13 17:39:25 +0000

    Update manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest -b r -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ayashaa-dumps/local-manifests.git --depth 1 -b bliss .repo/local_manifests
+git clone https://github.com/ayashaa/local-manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch bliss_ginkgo-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta # Put before last build command
-blissify -v ginkgo
+blissify -g ginkgo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
