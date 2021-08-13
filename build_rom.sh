b4f2703478a8d96130f7438c8f85f4dfa9a0e2c7
Endi Priyatna <65471980+Dreadwyrm@users.noreply.github.com>
2021-08-13 20:51:49 +0700

    Update dt for fix complication kernel

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch revengeos_ginkgo-user
+lunch revengeos_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
