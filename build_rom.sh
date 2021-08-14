ff3634105cfe66e8102a6db512367ed28e95a112
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-08-14 16:44:23 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/FlokoROM/manifesto.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/FlokoROM/manifesto.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/LynzhX/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+wget https://raw.githubusercontent.com/LynzhX/Patch/main/boot.patch -p external/selinux
+export TZ=Asia/Jakarta #put before last build command
+brunch RMX2001-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
