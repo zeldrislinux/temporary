f53663b690e0347133a818759540bfffcbc51110
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-08-15 11:54:18 +0700

    Fix Error on local_manifest

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 export ALLOW_MISSING_DEPENDENCIES=true
-. build/envsetup.sh
+source build/envsetup.sh
 export TZ=Asia/Jakarta #put before last build command
 brunch RMX2001
 
