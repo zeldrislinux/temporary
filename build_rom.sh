ff318c951e1ee62d3d23fb415f7ba1f4cb77184d
linux <bdiez19@gmail.com>
2021-08-15 12:38:06 -0300

    Initialbuild weebprojekt

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
-# Change to unofficial build.
-# Update to 5.1.3 https://twitter.com/dotosofficial/status/1424450801250885634
+# first initial build
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/linuxmobile/local_manifest --depth 1 -b dotos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/WeebProjekt/platform_manifest -b reborn -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/linuxmobile/local_manifest --depth 1 -b weeb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dot_chiron-userdebug
+lunch weeb_chiron-userdebug
 export TZ=Asia/Dhaka 
-make bacon -j$(nproc --all)
+make weeb-prod  -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
