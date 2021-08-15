8db87a08f11112fc4a08e1ef1cd3a04d93b5878a
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-15 08:09:19 +0700

    rolex: fix error build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lighthouse_rolex-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta
 mka lighthouse
 
