8c9bce6c7b1d4e29376f6e57d9e917132e8e1cce
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-08-16 00:00:15 +0600

    remove unnecessay lines

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-export KBUILD_BUILD_USER="ZHANtech"; export KBUILD_BUILD_HOST="gatotkaca"
-brunch mojito 2>&1 | tee ~/log/floko_$(date '+%Y%m%d_%H-%M-%S').log
+brunch mojito
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
