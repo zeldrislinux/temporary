d7c2f1fbe29f8db0fbf0ae8cceea4dc43f56becd
D34DPUL <77775456+D34DPUL@users.noreply.github.com>
2021-08-17 07:58:38 +0530

    Add Moto Camera

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/D34DPUL/local_manifest.git -b sakura --depth 1 -b main .repo/local_manifests
+git clone https://github.com/D34DPUL/local_manifest-sakura --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,9 +11,9 @@ mka bacon -j8
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 #rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/liber/boot.img cirrus:liber/sakura_16-8 -P 
-rclone copy out/target/product/liber/dtbo.img cirrus:liber/sakura_16-8 -P 
-rclone copy out/target/product/liber/system.img cirrus:liber/sakura_16-8 -P 
-rclone copy out/target/product/liber/product.img cirrus:liber/sakura_16-8 -P 
-rclone copy out/target/product/liber/vbmeta.img cirrus:liber/sakura_16-8 -P 
-rclone copy out/target/product/liber/vendor.img cirrus:liber/sakura_16-8 -P 
+rclone copy out/target/product/liber/boot.img cirrus:liber/sakura_17-8 -P 
+rclone copy out/target/product/liber/dtbo.img cirrus:liber/sakura_17-8 -P 
+rclone copy out/target/product/liber/system.img cirrus:liber/sakura_17-8 -P 
+rclone copy out/target/product/liber/product.img cirrus:liber/sakura_17-8 -P 
+rclone copy out/target/product/liber/vbmeta.img cirrus:liber/sakura_17-8 -P 
+rclone copy out/target/product/liber/vendor.img cirrus:liber/sakura_17-8 -P 
