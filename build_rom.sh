f672e5edbf6867d73e54acdde3c89c8fcc39297a
Shashank <9945shashank@gmail.com>
2021-08-18 23:05:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch stag_mido-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka stag
+make stag
 blissify -g -c mido
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
