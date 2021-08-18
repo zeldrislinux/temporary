22970032d2503887381908849fed3ddb6b4c6e9f
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-18 12:59:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch derp_RMX1831-userdebug
 export TZ=Asia/Dhaka #put before last build command
-brunch RMX1831
+mka derp
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
