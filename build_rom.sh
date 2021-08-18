8177a306e6d720e53b7e0d1fe5aa90a95d854bd3
Karan Parashar <whyredfire@gmail.com>
2021-08-18 12:18:13 -0700

    trigger: build vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export RADIANT_BUILD_TYPE=OFFICIAL
+export TARGETINCLUDEGAPPS=false
 lunch radiant_ginkgo-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
+
+cat out/target/product/ginkgo/system/build.prop | grep ro.system.build.date.utc=
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
