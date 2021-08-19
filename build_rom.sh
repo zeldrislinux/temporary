68d15f3fd28fff5bf901c281dc89aa8caf4ed42e
Asif <49162687+nhAsif@users.noreply.github.com>
2021-08-19 10:50:37 +0600

    Fix build error

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platf
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b rr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch rr_rosy-userdebug
