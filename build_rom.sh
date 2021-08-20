09e590f6d0e867516c0862d7843ab73f37681e9e
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-08-20 11:34:50 +0530

    Build: Test updated trees

--- build_rom.sh
+++ build_rom.sh
@@ -11,6 +11,6 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECK=true
 brunch violet
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line .)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line .
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/violet/*violet*.json cirrus:violet -P
