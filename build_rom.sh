58c810f6290ad8998d5c41bb2efdb6ebd4a5af77
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-08-20 08:42:05 +0700

    Fix selinux patch

--- build_rom.sh
+++ build_rom.sh
@@ -4,8 +4,8 @@ git clone https://github.com/LynzhX/local_manifest.git --depth 1 -b FlokoROM .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 brunch RMX2001
 
