006e466669e33a7073a33be2c830cdf194bc941d
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-21 23:11:00 +0530

    CIPHER OFFICIAL USER BUILD

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
+git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2020-userdebug
+lunch lineage_RMX2020-user
 export TZ=Asia/Dhaka #put before last  build command
 export CIPHER_MAINTAINER=sarthakroy2002
+export CIPHER_GAPPS=true
 export TARGET_FACE_UNLOCK_SUPPORTED=true
 export TARGET_USES_BLUR=true
 export CIPHER_OFFICIAL=true
