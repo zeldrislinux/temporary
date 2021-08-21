56364b8b0eb8af0243cb620058a05aaf2ce2bfef
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-08-21 11:06:50 +0530

    Passing test

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init -q --no-repo-verify --depth=1 https://github.com/Corvus-R/android_mani
 
 git clone https://github.com/Yasundram/local_manifest --depth 1 -b 30c .repo/local_manifests
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
