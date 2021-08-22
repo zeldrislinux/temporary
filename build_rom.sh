42213f8ba99456dca13b84e5efbb062d80ae031b
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-22 17:15:49 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch conquer_vince-user
+lunch conquer_vince-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make carthage
 
