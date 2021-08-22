ecf1abe8e2009273cc196538964acdb74f9f45a6
yash3056 <89018058+yash3056@users.noreply.github.com>
2021-08-22 20:23:08 +0530

    fix all error's

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vayu-userdebug
+lunch aosp_vayu-user
 export TZ=Asia/Dhaka #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
