a7bc99e687e1161ed0b6ddd9778fc16e2caa9507
Riot-X <abhiragh0@gmail.com>
2021-08-24 22:21:35 +0530

    Initial trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/P-404/platform_manifest.git -b rippa -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Th30utlaw/local_manifest.git --depth 1 -b p404 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Th30utlaw/local_manifest.git --depth 1 -b havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch p404_olives-userdebug
+lunch havoc_olives-user
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+brunch olives
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
