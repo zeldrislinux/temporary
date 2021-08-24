6502c5a19508368b9dc40013474a926bc14769a5
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-24 13:17:19 +0100

    Update to change local_manifest branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ConquerOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/rk134/local_manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/rk134/local_manifests.git --depth 1 -b conquer .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
