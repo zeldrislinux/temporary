6a8e9bef271ac9d6c685755fb76e2c3959f72758
tatannn <hangginifp@gmail.com>
2021-08-24 17:58:13 +0700

    Initial spark-os #2

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom.
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b fire -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/tatanstfu/local-manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
