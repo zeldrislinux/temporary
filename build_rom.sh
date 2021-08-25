5242f149c031de645e452c33a97ffed6ea2b3482
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-25 12:05:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kardebayan/local_manifests.git --depth 1 -b r11 .repo/local_manifests
+git clone https://github.com/Jamesgosling2004/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch potato_CPH1859-eng
+lunch potato_RMX1831-userdebug
 export TZ=Asia/Kolkata #put before last build command
-brunch CPH1859
+brunch RMX1831
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
