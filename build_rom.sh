158dcb9a2956e007a7c37b81b2ff0b3a82e03cd9
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-08-25 09:14:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manife
 git clone https://github.com/flashokiller/manifest_pine -b radinat --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build romhoul
 source build/envsetup.sh
 lunch radiant_pine-userdebug
 export TZ=Asia/Kolkata #put before last build command
