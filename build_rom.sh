dff450670eddb94947d880b064ffdfa46bf03865
Unknownbitch <68426327+Unknownbitch07@users.noreply.github.com>
2021-08-27 08:39:17 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Unknownbitch07/local_manifest.git --depth=1 -b ssos11 .repo/local_manifests
+repo init --depth=1 -u git://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Unknownbitch07/local_manifest.git --depth=1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
-source build/envsetup.sh
-lunch ssos_tissot-userdebug
+. build/envsetup.sh
+lunch xdroid_tissot-userdebug
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+make xd
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
