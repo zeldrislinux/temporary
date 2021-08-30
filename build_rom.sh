e19ba42710e97d8e01b8295f154f5856490cf544
linux <bdiez19@gmail.com>
2021-08-30 19:17:09 -0300

    Nuked livedisplay

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/linuxmobile/local_manifest --depth 1 -b nusantara .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 # build rom 
 source build/envsetup.sh
 lunch nad_chiron-userdebug
