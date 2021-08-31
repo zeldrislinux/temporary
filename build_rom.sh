f6da005603a877f8195a5aa6eb7ddfc807c43eb1
NganuCoeg <starterpackmeh@gmail.com>
2021-08-31 02:22:12 +0000

    August Changelogs

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/NganuCoeg/local_manifests --depth 1 -b r5x-BananaDroid .repo/local_manifests
+git clone https://github.com/NganuCoeg/local_manifests --depth 1 -b r5x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
