eedb9625924f58a1e8d68799ff1cbe28c978f979
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-31 09:01:53 +0530

    Initial Release Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/sarthakroy2002/local_manifest --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2020-userdebug
+lunch aosp_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
