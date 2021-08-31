ab704bad3fd50fd9da4405dc0c4985c90ca65a85
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-31 22:53:19 +0700

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b nusantara .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 10 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b nusaS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch nad_rolex-userdebug
+lunch nad_rolex-user
 export TZ=Asia/Jakarta
 export KBUILD_USERNAME=ItoRenz
 mka nad
