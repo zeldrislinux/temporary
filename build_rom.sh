ae234b8768bd4fcb12744e5d76745a2df5d481f8
xoo2001 <ekora2001@gmail.com>
2021-08-31 07:16:02 +0700

    ido: NusantaraProject

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b nad-r2 .repo/local_manifests
+git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b nad .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_wt88047x-userdebug
+lunch nad_ido-userdebug
 export TZ=Asia/Jakarta
 mka nad
 
