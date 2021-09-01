cf012e9819a105b5e63c11e166fa24808ac84ac8
popoASM <58354320+popoASM@users.noreply.github.com>
2021-09-01 12:12:16 +0530

    holland1: fix build error

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_holland1-usedebug
+lunch du_holland1-usedebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
