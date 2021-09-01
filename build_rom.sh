a93ff7f06a4bc2cde38f898bf1b857e818a24409
popoASM <58354320+popoASM@users.noreply.github.com>
2021-09-01 14:24:28 +0530

    holland1: ALLOW_MISSING_DEPENDENCIES=true

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch du_holland1-usedebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 make corvus
 
