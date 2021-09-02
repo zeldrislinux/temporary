cda5ef3917d74e2b9e7e6604806843a2339a7d14
linux <bdiez19@gmail.com>
2021-09-02 08:56:07 -0300

    Cherish Os 2.9

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manif
 git clone https://github.com/linuxmobile/local_manifest --depth 1 -b linuchin .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch cherish_chiron-userdebug
 export TZ=Asia/Dhaka 
