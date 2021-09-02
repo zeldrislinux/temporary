ffea65aea61c5eae0f6d01cec3a971179a100c25
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-09-02 20:49:50 +0700

    Fix build error

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/LynzhX/local_manifest/blob/blast/local_manifest.xml --depth 1 -b blast .repo/local_manifests
+git clone https://github.com/LynzhX/local_manifest.git --depth 1 -b blast .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
