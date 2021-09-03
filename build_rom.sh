5b1d2530c7707260fc892901e6c37ecd89ba526e
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-03 16:30:06 +0530

    fix issue

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,6 @@ lunch aosp_raphael-userdebug
 export TZ=Asia/Kolkata
 export ALLOW_MISSING_DEPENDENCIES=TRUE #put before last build command
 make bacon
-# upload rom 
+
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip.json cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
