282ce783e7e0b922d98256a70e630f7ff16bb9e2
linux <bdiez19@gmail.com>
2021-09-03 18:05:54 -0300

    Added Performance Control

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifes
 git clone https://github.com/linuxmobile/local_manifest --depth 1 -b pixelx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_chiron-userdebug
 export TZ=Asia/Dhaka 
