45c313e6dc79818096503e1936cb89ba2db2a4f3
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-03 16:08:00 +0530

    update multi file up

--- build_rom.sh
+++ build_rom.sh
@@ -11,3 +11,4 @@ export ALLOW_MISSING_DEPENDENCIES=TRUE #put before last build command
 make bacon
 # upload rom 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip.json cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
