784fe5e97e96db1c76b48b67bc0f11f6e92d52a3
Asif <49162687+nhAsif@users.noreply.github.com>
2021-09-04 08:42:45 +0600

    fix error

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch p404_rosy-userdebug
 export TZ=Asia/Dhaka
-mka bacon
+make p404
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
