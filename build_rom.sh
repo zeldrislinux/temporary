5e56e8758f506b052c2afdd087c06248b6580679
nhAsif <najmulhasan3609@gmail.com>
2021-09-04 09:54:11 +0600

    init build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b p404 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ConquerOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b conquer .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch p404_rosy-userdebug
+lunch conquer_rosy-userdebug
 export TZ=Asia/Dhaka
-mka bacon
+make carthage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
