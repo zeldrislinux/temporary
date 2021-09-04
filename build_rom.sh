3e08e4a7c36d2a884b717c6ad55b78fb92562fdb
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-09-04 17:04:41 +0700

    Rom Update

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b ProjectSakura-11 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 
 # build rom
 source build/envsetup.sh
