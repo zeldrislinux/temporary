1393ce661097de99b4d5402be35058cf1796dd49
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-09-05 21:54:40 +0530

    userdebug build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_tiare-user
+lunch arrow_tiare-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
