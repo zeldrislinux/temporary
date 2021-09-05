cbe211daa001fb808636dabc842ac44b73ee2b70
popoASM <58354320+popoASM@users.noreply.github.com>
2021-09-05 18:03:57 +0530

    miatoll: spark initial unofficial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/atoll-popo/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest -b fire -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/atoll-popo/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosip_miatoll-userdebug
+lunch spark_miatoll-userdebug
 export TZ=Asia/Kolkata #put before last build command
-export WITH_GAPPS=true
-time m kronic
+mka spark
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
