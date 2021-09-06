e4c98dd7eeeb2a6c32d1d40bf90950938741d6f3
Debayan Kar <69067988+kardebayan@users.noreply.github.com>
2021-09-07 00:44:50 +0530

    append time with zip
    * also trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_CPH1859-eng
+export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 mka bacon -j$(nproc --all)
