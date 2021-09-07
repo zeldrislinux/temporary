f786a7eddec23ed12585b5cfd4a5c63ba31e873b
Nischay Sanjay <70051520+IceBreaker2451@users.noreply.github.com>
2021-09-07 20:31:44 +0530

    GApps Build

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export BUILD_WITH_GAPPS=true
 lunch pixys_tulip-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make pixys
