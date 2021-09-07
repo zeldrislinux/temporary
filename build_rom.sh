6cc21f0539aaec0fe8e9a24c0439b6a46784e678
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2021-09-07 14:37:50 +0530

    remove || from sync line

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-device,-mips,-darwin,-notdefault # Initilizeing dotOS11 sources
 git clone https://github.com/RahulPalXDA/local_manifest.git --depth=1 -b pex_holland2 .repo/local_manifests # adding builder's local_manifest to Initilized sources
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 # Syncing previously Initilized sources
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 # Syncing previously Initilized sources
 
 # build rom
 source build/envsetup.sh
