20898c0949871dc4c76cf2cbd8656216cf93dd81
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-10 00:39:14 +0530

    initial evo build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/RevengeOS/android_manifest.git -b r11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b X01AD .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b evolution .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch revengeos_X01AD-userdebug
+lunch evolution_X01AD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=TRUE
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
