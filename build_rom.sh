35b53acd1e5b7b096b23b74ab1016a6a38b44f05
Harshhaa Reddy <79190327+harshhaareddy@users.noreply.github.com>
2021-09-09 13:35:34 +0530

    X00TD: initial los build

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch lineage_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_API_CHECKS=true
 export TARGET_FLOS=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last build
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
