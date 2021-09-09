99b525384a7af3f2b9b3dce5d92191f5a49d327d
Nischay Sanjay <70051520+IceBreaker2451@users.noreply.github.com>
2021-09-09 09:03:04 +0530

    Build GApps Variant

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosip_tulip-userdebug
+export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 time m kronic
 
