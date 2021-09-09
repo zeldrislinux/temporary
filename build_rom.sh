ac1bc94e944f0e0e13492034e8e92ff2ddc7a30e
Nischay Sanjay <70051520+IceBreaker2451@users.noreply.github.com>
2021-09-09 10:53:35 +0530

    Use 4.19 kernel

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PotatoProject/manifest -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/IceBreaker2451/potato_local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/IceBreaker2451/local_manifest --depth 1 -b potato .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
