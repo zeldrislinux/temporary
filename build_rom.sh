fc57c0308c974a5e05583dd6a5f09e4058c15927
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-10 08:25:22 +0530

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,10 @@
-repo init --depth=1 --no-repo-verify -u git://github.com/HyconOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelBlaster-OS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
-lunch aosp_X01BD-userdebug
-export HYCON_BUILD_TYPE=OFFICIAL #put before last build command
+lunch aosp_X01BD-userdebug 
 export ALLOW_MISSING_DEPENDENCIES=TRUE
 mka bacon
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip.json cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
