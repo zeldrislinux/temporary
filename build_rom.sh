91896da00805a71775b2e70c15cbd0bb6316dfcd
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-10 23:09:23 +0530

    initial evo build for max pro m2

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b evolutionpro .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
-lunch evolution_X01DB-userdebug
+lunch evolution_X01BD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=TRUE
 export SKIP_ABI_CHECKS=TRUE
 mka evolution
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip.json cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
