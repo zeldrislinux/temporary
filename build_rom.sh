29224557c293807bd900a92dab75b94da13699c3
Sarthak Roy <sarthakroy2002@gmail.com>
2021-09-10 14:26:28 +0530

    fix kernel

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/hentaiOS/platform_man
 git clone https://github.com/sarthakroy2002/local_manifest -b hentai --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build  rom
 source build/envsetup.sh
 lunch hentai_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
@@ -15,5 +15,5 @@ export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES=true
 make otapackage -j$(nproc --all)   
 
-# upload  rom
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
