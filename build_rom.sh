f3bbc450acc5e72c9a01ea2577e329bfe03c3fc6
Rabin <60746629+rabin-tech@users.noreply.github.com>
2021-09-12 08:32:48 +0545

    Test wfd

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export HYCON_BUILD_TYPE=OFFICIAL
 lunch aosp_tulip-userdebug
+export TZ=Asia/Kathmandu #put before last build command
 mka bacon
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
