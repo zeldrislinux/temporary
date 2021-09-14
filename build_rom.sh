76d2e982273cd8b4de63a041f59e82c2807f8bec
PinCredible <86541065+PinCredible@users.noreply.github.com>
2021-09-14 16:16:34 +0530

    update [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest.git -b ace -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/PinCredible/local-manifest.git --depth 1 -b nogapps .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/PinCredible/local-manifest.git --depth 1 -b dot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_tulip-user
+lunch dot_merlin-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
 
