51c240ca6bbee1081ecc2e7b65c07c92412e31ed
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-15 12:35:35 +0530

    add some patches

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
-repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b octavius .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
 lunch octavi_X01BD-userdebug
  #put before last build command
+export TZ=Asia/Kolkata
 export ALLOW_MISSING_DEPENDENCIES=TRUE
+export SKIP_ABI_CHECKS=TRUE
 brunch X01BD
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
