c364106db77b2bf8f9ecd5f69fdd89e85e3ef4f5
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-15 09:04:08 +0530

    X01BD : Octavi Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
-repo init --depth=1 --no-repo-verify -u git://github.com/HyconOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b octavius .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
-lunch aosp_X01BD-userdebug
-export HYCON_BUILD_TYPE=OFFICIAL #put before last build command
+lunch octavi_X01BD-userdebug
+ #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=TRUE
-mka bacon
+brunch X01BD
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip.json cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
