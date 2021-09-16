e4c0026857c553145afb4ea8485152295b9c71ad
PinCredible <86541065+PinCredible@users.noreply.github.com>
2021-09-17 04:42:26 +0530

    Trying to fix sync issue

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,8 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/PinCredible/local-manifest.git --depth 1 -b dot .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 
 # build rom
 source build/envsetup.sh
