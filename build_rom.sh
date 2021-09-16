a7114a4c935cf9f4aee363f584b8c7284d82870d
PinCredible <86541065+PinCredible@users.noreply.github.com>
2021-09-17 04:03:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch dot_merlin-user
+lunch dot_merlinx-user
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
