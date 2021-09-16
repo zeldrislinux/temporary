e9cadc63ccb8810853a83bf71893057953a8a116
xoo2001 <ekora2001@gmail.com>
2021-09-16 19:00:54 +0700

    ido: Re branch Android11

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 10 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b arrow-10.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
