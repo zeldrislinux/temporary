45e43836cb5752f8bf1999f2c6550a5d3ea028ed
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-09-17 08:07:24 +0530

    SEPTEMBER UPDATE

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Yasundram/local_manifest --depth 1 -b octavi .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30
 # build rom
 
 source build/envsetup.sh
