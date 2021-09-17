e66a35a944288944af8d195b04076eb173f03b3a
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-09-18 06:36:05 +0700

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://gitlab.e.foundation/e/os/android.git -b v1-r -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/LynzhX/local_manifest.git --depth 1 -b e .repo/local_manifests
+git clone https://github.com/LynzhX/local_manifest.git --depth=1 -b eOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
