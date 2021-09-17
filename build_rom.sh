4bb940dcf8a4b9f23c26bd8016e77ab0e165141b
xoo2001 <ekora2001@gmail.com>
2021-09-17 07:12:01 +0700

    ido: fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 source build/envsetup.sh
 lunch lineage_ido-userdebug
