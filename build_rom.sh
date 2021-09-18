18b299c2eabbd026c2e1adb2465098b86f7b3c5f
AbrarNoob <70853665+AbrarNoob@users.noreply.github.com>
2021-09-18 23:23:22 +0600

    Try to fix some things

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifes
 git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b komodo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch komodo_lavender-userdebug
