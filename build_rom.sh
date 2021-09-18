d072e8f6eeb97a1ae4a4f39b37e817e5d95bf59a
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-18 12:48:09 +0530

    Final build for fixing hals

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manife
 git clone https://github.com/N4veenNK/local-manifest.git --depth 1 -b legion .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#build rom
+# build rom
 source build/envsetup.sh
 lunch legion_X00TD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
