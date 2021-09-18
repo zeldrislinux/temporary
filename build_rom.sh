f576f6f32778cf0265f60f459cef764de1f58e97
SriBalaji <86469621+iamthecloverly@users.noreply.github.com>
2021-09-18 11:39:08 +0530

    RMX2001: los fe

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/iamthecloverly/local_manifest.git --depth 1 -b Los-FE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
