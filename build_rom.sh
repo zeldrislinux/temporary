37f90017c74d6c56b09e0a2edf7653bf30e76522
Aryan Sinha <techyminati@cipheros.site>
2021-09-18 21:00:02 +0530

    olivelite: Initial Trigger forScratch Olivelite Bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 
-git clone https://github.com/MinatiScape/local_manifest --depth=1 .repo/local_manifests
+git clone https://github.com/MinatiScape/local_manifest -b olivelite --depth=1 .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build
 source build/envsetup.sh
-lunch lineage_tiare-user
+lunch lineage_olivelite-eng
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
 
 # upload build
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-# Lets get build datetime (UTC) for pushing OTA
-cat out/target/product/tiare/system/build.prop | grep "ro.build.date.utc"
+# Upload recoveryimage
+rclone copy out/target/product/olivelite/recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
