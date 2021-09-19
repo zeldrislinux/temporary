42ad0e31f421c4484b1c5ae2a62f7882b66f0d32
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2021-09-19 12:18:48 +0600

    update build.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b r11.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Hotorou/local.git --depth 1 -b aicp .repo/local_manifests
+git clone https://github.com/ZunayedDihan/local.git --depth 1 -b aicp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
