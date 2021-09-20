695ecf4636df3d83aa9ba265733de5a272542fe4
xoo2001 <ekora2001@gmail.com>
2021-09-20 12:22:50 +0700

    X682C: fix source booted

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Infinix-MT6768-LDM/local_manifests.git --depth 1 -b 11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 10 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Infinix-MT6768-LDM/local_manifests.git --depth 1 -b 10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
