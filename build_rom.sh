882748ff54d1f93a2c9ee170d9dc97600daadf9e
ImFlynnn <ahmadzaid13.az@gmail.com>
2021-09-21 02:33:34 +0000

    Try fix safetynet

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch arrow_RMX2001-userdebug
-export ARROW_GAPPS=true
 export TZ=Asia/Jakarta #put before last build command
 m bacon
 
