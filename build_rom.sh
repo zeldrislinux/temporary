28dce563ca238235a1ad54016f38a97d2ba76b5b
AbrarNoob <70853665+AbrarNoob@users.noreply.github.com>
2021-09-22 16:29:32 +0600

    Test: Changed DT

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b wave .repo/local_manifests
+git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 source build/envsetup.sh
+export VANILLA_BUILD=true
 lunch wave_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
