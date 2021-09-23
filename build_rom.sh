7a3dd97f8361bc72baeb586ce192264eaed3c7da
wHoEMi <ehteshammalik18998@gmail.com>
2021-09-23 23:42:22 +0530

    rm -j flags

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,7 @@ source build/envsetup.sh
 lunch p404_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-threads=-j$(( $(nproc )/2 | bc )) # use half cpu cores only
-make bacon $threads
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
