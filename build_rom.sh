e514392ff73f394e8c89b4ee5853a06c6e4a25c1
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-23 19:49:30 +0530

    Fix Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/exthmui/android.git -b 
 git clone https://github.com/N4veenNK/local-manifest.git --depth 1 -b exthm .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#  build rom
+# build rom
 . build/envsetup.sh
 lunch exthm_X00TD-userdebug
 export TARGET_GAPPS_ARCH=arm64
