2a0cd3698500cf303115e32302e332015f5bda3d
officialputuid <officialputuid@hack.id>
2021-09-25 18:08:53 +0800

    temporary: Fix derp

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/exthmui/android.git -b 
 git clone https://github.com/officialputuid/RMX2001-exTHmUI --depth 1 -b MASTER .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom ****
+# build rom
 source build/envsetup.sh
 export EXTHM_GAPPS=true
 export TARGET_GAPPS_ARCH=arm64
