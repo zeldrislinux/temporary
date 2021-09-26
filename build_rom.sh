df4fab11ba446877dfadb48bfb276fc71d686806
Asim Ranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-26 17:41:13 +0500

    switch to userdebug for xiaomi parts

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/a7imranjha/local_manifest.git --depth 1 -b des .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j30
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch descendant_olivewood-user
+lunch descendant_olivewood-userdebug
 export TZ=Asia/Karachi #put before last build command
 mka descendant
 
