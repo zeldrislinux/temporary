073261fba4b97e79f00ddd3fe7dc8bacb3b092a7
Nischay Sanjay <70051520+IceBreaker2451@users.noreply.github.com>
2021-09-26 08:08:12 +0530

    Initial build with 4.19 kernel

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch nad_tulip-userdebug
-export USE_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 mka nad
 
