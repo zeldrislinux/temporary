ffc33084bf1b9bf05c129276fa90f646ab6d725c
Asim Ranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-27 13:28:55 +0500

    switch to new trees

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/a7imranjha/local_manifest.git --depth 1 -b des .repo/local_manifests
+git clone https://github.com/a7imranjha/local_manifest.git --depth 1 -b desc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
