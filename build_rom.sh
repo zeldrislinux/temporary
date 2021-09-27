3d17c84e4089ffcf3592505516ed1c5da65bf190
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-27 15:27:14 +0530

    Initial CrDroid Build for X01BD with 4.19Kernel

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
-repo init --depth=1 --no-repo-verify -u git://github.com/HyconOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b crdroid4.19 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
-lunch aosp_X01BD-userdebug
-#export HYCON_BUILD_TYPE=OFFICIAL
 export ALLOW_MISSING_DEPENDENCIES=TRUE
-mka bacon
+export TZ=Asia/Kolkata 
+brunch X01BD
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip.json cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
