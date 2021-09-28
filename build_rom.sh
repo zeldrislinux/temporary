9ef269edae5d00bd7c12b2dab9814b352165e425
Karan Parashar <whyredfire@gmail.com>
2021-09-28 12:01:27 +0530

    eleven: fix smart charging

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/PixelOS-Pixelish/manifest -b eleven --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/PixelOS-ginkgo/local_manifests.git --depth 1 -b eleven .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
