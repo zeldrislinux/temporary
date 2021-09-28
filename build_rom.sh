4fa07e2aa0a058a45a6cf89b1ca37b945e7e5555
Asim Ranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-29 00:32:53 +0500

    Gapps build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch descendant_olivewood-user
+export WITH_GAPPS=true
 export TZ=Asia/Karachi #put before last build command
 mka descendant
 
