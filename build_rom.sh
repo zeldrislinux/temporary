453e5295527af0837143134cedfbff3a02bb798c
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-09-29 00:04:48 +0530

    P404-Z01R With Gcc Track

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,5 @@ lunch p404_Z01R-user
 export TZ=Asia/Kolkata 
 make bacon -j$(nproc --all)
 
-
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
