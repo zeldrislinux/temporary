bf4e5f327c244bf272a72aa66fd6e92441f2b3b6
Asim Ranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-29 14:01:55 +0500

    build descendant with new trees

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch descendant_olivewood-user
-export WITH_GAPPS=true
 export TZ=Asia/Karachi #put before last build command
 mka descendant
 
