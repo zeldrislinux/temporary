88cb12ff69c0d518d177f7003612d5eac2371b84
Asim Ranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-29 14:16:33 +0500

    switch to new trees

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch descendant_olivewood-userdebug
+lunch descendant_olives-user
 export TZ=Asia/Karachi #put before last build command
 mka descendant
 
