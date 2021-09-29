8933f51c8c2b126cd1faccd50065ef1ce1aad292
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-09-29 07:19:54 +0530

    final build for bliss rom

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-blissify options olivewood
+blissify olivewood
 export TZ=Asia/Dhaka #put before last build command
 
 
