8bff9f04ed3afb6fd4e9e7619142edd9ab81cebd
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-10-01 21:44:04 +0530

    vendor Z01R

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xperience_Z01R-userdebug
-#export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata 
 #make bacon -j$(nproc --all)
 make vendorimage
