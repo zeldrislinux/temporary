613ab0883915641d99447c01ac054a04b3df1eee
Debayan Kar <69067988+kardebayan@users.noreply.github.com>
2021-10-02 00:52:24 +0530

    switch to userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_CPH1859-eng
+lunch lineage_CPH1859-userdebug
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
