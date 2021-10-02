5628b8dc190edcfb5026f16b818b8cf13bb2e3dc
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-10-02 10:27:51 +0530

    Final build

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LessAosp/manifest.git
 git clone https://github.com/ASHISH11948/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 repo sync —force-sync packages/apps/Mantle
+chmod a+x vendor/aosp/tools/generate_json_build_info.sh
 
 # build rom
 . build/envsetup.sh
