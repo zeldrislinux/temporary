3ef59419030bc7883e1ed09469ea809e260df937
Karan Parashar <whyredfire@gmail.com>
2021-10-02 18:25:51 +0530

    eleven: fixed kcal and media player

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom
 repo init -u https://github.com/PixelOS-Pixelish/manifest -b eleven --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/PixelOS-ginkgo/local_manifests.git --depth 1 -b eleven .repo/local_manifests
+git clone https://github.com/PixelOS-ginkgo/local_manifests.git --depth 1 -b r-player .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
 lunch aosp_ginkgo-user
 export TZ=Asia/Dhaka #put before last build command
-make SystemUI
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-# rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-# upload SystemUI
-rclone copy out/target/product/ginkgo/system_ext/priv-app/SystemUI/SystemUI.apk cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
