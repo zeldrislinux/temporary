6f63a7cf6adf592fd0b9a463ac17954840fea188
Asim Ranjha <65493194+a7imranjha@users.noreply.github.com>
2021-10-02 15:14:54 +0500

    Added XiaomiParts , Added MiuiCam, Fixed WFD

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/a7imranjha/local_manifest.git --depth 1 -b pexia .repo/local_manifests
+git clone https://github.com/a7imranjha/local_manifest.git --depth 1 -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_olivewood-userdebug
+lunch aosp_olivewood-user
 export TZ=Asia/Karachi #put before last build command
 mka bacon
 
