7986ee1880c68910a3681c272130ddf5bcd22fd0
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-10-03 07:30:01 +0530

    Added September patches

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LessAosp/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LessAosp/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ASHISH11948/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 repo sync —force-sync packages/apps/Mantle
@@ -9,7 +9,7 @@ chmod a+x vendor/aosp/tools/generate_json_build_info.sh
 . build/envsetup.sh
 lunch aosp_olivewood-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon -j16 |tee log
+make bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
