c01c4aabc98e04cb79e5f3dad9069729123f3cff
boedhack99 <boedhack@gmail.com>
2021-10-04 01:09:12 +0700

    Trigger: Switch to stock perf

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_mojito-userdebug
+lunch p404_mojito-eng
 export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Bangkok #put before last build command
