ea5342b184b0880bee469375267f1579cf83dcc3
ben863 <r.budhiono@gmail.com>
2021-10-03 13:28:06 +0700

    Project Sakura: initial Unofficial build for X00TD
    Change-Id: I70c020a705b1f8d3ca08c57eef9bfa9a1cb814ae

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b 17.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b sakura .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
