115571a1267f48ed8c822ef47c4332070319e4c5
vesh <veshrao@protonmail.com>
2021-10-04 13:39:04 +0800

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/veshrao/local_manifest.git --depth 1 -b xdroid .repo/local_manifests
+git clone https://github.com/veshrao/local_manifest.git --depth 1 -b rovacaf .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
