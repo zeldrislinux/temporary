51cc5208e5b9c647d9e92fc3dcf7fb43cfe249c2
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-06 18:17:45 +0100

    vince: build cipherOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,20 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/rk134/local_manifests.git --depth 1 -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch lineage_vince-eng
+export CIPHER_MAINTAINER=r_k87
+export CIPHER_GAPPS=true
+export TARGET_FACE_UNLOCK_SUPPORTED=true
+export TARGET_USES_BLUR=true
+export CIPHER_OFFICIAL=false
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
