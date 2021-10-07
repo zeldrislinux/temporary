3092cd98e9e0abb89aa4b2136e1a7d866bdaa2f3
Harshhaa Reddy <79190327+harshhaareddy@users.noreply.github.com>
2021-10-07 13:24:02 +0530

    X00TD: xdroid 1.6.5 build

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put xdroid on board
 make xd
 
