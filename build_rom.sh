ad91ca53c7fe830081d8ccba26605361873df876
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-10-09 11:22:25 +0700

    Specified oss treed official

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b nusa .repo/local_manifests
+git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b nusan .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch nad_rova-user
+lunch nad_rova-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_USERNAME=ItoRenz
+export KBUILD_BUILD_USER=ItoRenz
+export KBUILD_BUILD_HOST=Tempest
 mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
