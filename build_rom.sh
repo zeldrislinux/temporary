91b32e37437eda37d7eab161df70a74d64172475
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-10-09 19:41:44 +0700

    G35: NusantaraUpdate ( Fix Error build)

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+WITH_GAPPS=true
 lunch nad_RMX2185-userdebug
 mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-
