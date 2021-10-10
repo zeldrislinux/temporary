3d504c31b75077077174fc8a44e723c1c0513176
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-10-11 02:42:38 +0700

    arrow-12.0

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b Arrow-12.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-device,-mips,-darwin,-notdefault
+
+git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b arrow .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
+
 lunch arrow_RMX1941-userdebug
-export TZ=Asia/Dhaka #put before last build command
+
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
