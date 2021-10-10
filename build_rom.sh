921380123eb20802cc62f24772f3610a618efad5
wHoEMi <ehteshammalik18998@gmail.com>
2021-10-11 02:19:33 +0530

    initial aex a12 build

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b lineage-18.1-oss .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/wHo-EM-i/manifest.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_rova-userdebug
-export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=ItoRenz
-export KBUILD_BUILD_HOST=Tempest
-mka bacon
+lunch aosp_lavender-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Kolkata #put before last build command
+export KBUILD_BUILD_USER=wHoEMi
+export KBUILD_BUILD_HOST=ehtesham
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
