57eedb4fbb5e0cbc8905afd6695be62a923d2c07
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-10-10 09:08:49 +0530

    October Build

--- build_rom.sh
+++ build_rom.sh
@@ -2,10 +2,10 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b evolutionpro .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+#octoberBuild
 . build/envsetup.sh
 lunch evolution_X01BD-userdebug
-export ALLOW_MISSING_DEPENDENCIES=TRUE
+export ALLOW_MISSING_DEPENDENCIES=TRUE 
 export SKIP_ABI_CHECKS=TRUE
 mka evolution
 
