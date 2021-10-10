dd98bd355bdf845675c06f054807a6a09f5543d3
Danish メ⁬⁮⁮⁮⁮ <adamzulkey@gmail.com>
2021-10-11 06:25:54 +0800

    ulova: fixed manifest

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/mani
 git clone https://github.com/Dannoob/local_manifests.git --depth 1 -b eleven-ulova .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_ulova-user
 export TZ=Asia/Malaysia #put before last build command
