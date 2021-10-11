a12a8b4215660fda394c260f4e363ff5e7239e8d
Kitsunejasutin <73803767+Kitsunejasutin@users.noreply.github.com>
2021-10-11 15:37:06 +0800

    trigger: Vanilla Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11 --depth=1 
 git clone https://github.com/Kitsunejasutin/local_manifest.git --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 export WITH_GAPPS=true
 lunch octavi_ginkgo-user
