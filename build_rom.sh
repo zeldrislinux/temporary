89d6cd5da68e8e5d16ede67a553c0cbab3af9b16
amier_san09 <73158213+MhmmdAmier@users.noreply.github.com>
2021-10-12 21:39:10 +0700

    fix bt crash

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/MhmmdAmier/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ZilverQueen/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch banana_ulysse-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
 make banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
