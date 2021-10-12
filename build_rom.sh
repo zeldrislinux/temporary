8578d62dd988a76ad87b6cd3aab817a98f4a1ab0
NganuCoeg <starterpackmeh@gmail.com>
2021-10-12 09:01:44 +0800

    fix fail build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch xdroid_r5x-userdebug
+lunch xdroid_r5x-eng
 export TZ=Asia/Jakarta
 make xd
 
