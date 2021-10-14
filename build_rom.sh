2f1eab90c0182197cadf216cacc40c68b7e5abf4
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-14 10:05:39 +0100

    vince: ignore neverallow [TEMP]

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_vince-eng
+export SELINUX_IGNORE_NEVERALLOWS=true
+export KBUILD_BUILD_USER=rxhul
+export KBUILD_BUILD_HOST=android-lab
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
