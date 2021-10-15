8b83583177cffaf4cc7d2c250841a5bc7be1cec9
AlissonGrizotti <alisson1397@hotmail.com>
2021-10-14 22:31:35 -0300

    Build LegionOS for Vayu

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Alissongrizotti/local_manifest.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manifest.git -b 12 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Alissongrizotti/local_manifest.git --depth 1 -b legion .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vayu-userdebug
-m aex
+lunch legion_vayu-userdebug
+make legion
 # build
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
