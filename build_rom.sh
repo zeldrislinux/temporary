05a48f7eef60e387824d41e83e9ae846784eb35e
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-10-18 04:53:38 +0530

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_X00TD-userdebug
+lunch aosp_X01BD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
