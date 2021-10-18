0f84bda16a09e05b1135bc143592245ad8c35bb5
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2021-10-18 11:25:03 +0600

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Palladium-OS/platform
 git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b pal .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch palladium_lavender-userdebug
