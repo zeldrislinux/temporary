82dbfac7c2d746aa7e4ea64c1210f9c8de997328
PrajjuS <theprajjus@gmail.com>
2021-10-19 17:06:01 +0530

    vince-ProjectSakura-PrajjuS: Fixup repo init
    Change-Id: I2203d5db27e7ae56042ea6248e15dd10416bd6ed
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/PrajjuS/local_manifest_vince.git --depth 1 -b sakura .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
