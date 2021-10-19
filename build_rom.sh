459f24dfce026cc41e42ca05a6e96c63d6685253
Razs <73821326+rasenss@users.noreply.github.com>
2021-10-19 07:08:53 +0700

    Try build gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch palladium_ginkgo-userdebug
-export WITH_GAPPS=true
+export PALLADIUM_BUILD_VARIANT := GAPPS
 export TZ=Asia/Dhaka #put before last build command
 mka palladium
 
