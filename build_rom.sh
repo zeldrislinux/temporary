dc2a8f8f59ae40bbb580f4dc8512cf0ad102f943
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-10-19 08:43:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b rippa -g default,-mips,-darwin,-notdefault
 git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth=1 -b 404 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1 --fail-fast
 
 # build rom
 source build/envsetup.sh
@@ -12,5 +12,4 @@ make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/Z01R/vendor.img cirrus:Z01R -P
- 
+#rclone copy out/target/product/Z01R/vendor.img cirrus:Z01R -P
