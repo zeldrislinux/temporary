4124ef631bbb28b5ec0b3ea11d42e4d51c15e11a
ECr34T1v3 <eriikgarciia1999@gmail.com>
2021-10-19 00:05:17 -0300

    beyond0lte: Build AOSPK (test-release_02-19_10)

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AOSPK/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ECr34T1v3/android_.repo_local_manifests.git --depth 1 -b beyond0-aospk .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPK/manifest -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ECr34T1v3/android_.repo_local_manifests --depth 1 -b beyond0-aospk .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
