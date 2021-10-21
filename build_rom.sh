2e0f0b9c90f3204c763eeae382da00baa26eba09
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-10-21 19:00:04 +0700

    First build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/WeebProjekt/platform_manifest -b reborn -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/WeebProject/platform_manifest -b sushi -g default,-mips,-darwin,-notdefault
 git clone https://github.com/LynzhX/local_manifest.git --depth 1 -b Weeb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
