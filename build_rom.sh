66e0f82193dc1589e42a630cdbd343361b13b497
Nischay Sanjay <70051520+IceBreaker2451@users.noreply.github.com>
2021-10-22 08:42:08 +0530

    tulip: switch to decommonized trees

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-repopick 14770
-repopick -t twelve-sepol
-repopick -t twelve-monet
 lunch arrow_tulip-userdebug
 export ARROW_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
