fa0be27c9ab6e0083de7bb94e050cc3272017ca4
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-10-22 14:45:08 +0530

    removes jl fail fast

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,6 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b common .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-repo sync -j1 --fail-fast
 
 # build rom
 . build/envsetup.sh
