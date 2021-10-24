6fce023f6a4deb039c81d89588f7196d4acaec0b
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-10-24 06:00:22 +0530

    Fixed some booting issues

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch corvus_olivewood-userdebu
+lunch corvus_olivewood-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make corvus
 
