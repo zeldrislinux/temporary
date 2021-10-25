56530f66a284c5bfe726c68ba8fc105a066f6606
Aryan Karan <aryankaran28022004@gmail.com>
2021-10-25 12:22:07 +0530

    It's lunch not brunch

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,8 @@ source build/envsetup.sh
 export KBUILD_BUILD_USER=aryan
 export KBUILD_BUILD_HOST=aryankaran
 export TZ=Asia/Kolkata
-brunch evolution_onclite-user
+lunch evolution_onclite-user
+m
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
