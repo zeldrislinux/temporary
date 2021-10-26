b5d63a37bec107e7b36d45c4d374c248142da0b9
Flasho <75789819+flashokillerify@users.noreply.github.com>
2021-10-26 08:50:09 +0530

    Monte

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://GitHub.com/flashokillerify/manifest_pine --depth 1 -b A12 .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b A12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom....
