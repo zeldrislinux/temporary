be5101073f5aaccd8c519fe217e4413e5fb40331
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-10-26 17:51:09 +0530

    New update for nusantra

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ASHISH11948/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ASHISH11948/local_manifest.git --depth 1 -b ashish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch nad_olivewood-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka nad
 
