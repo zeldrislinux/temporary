aec177862dd874bc8ce970a3e6dc28408eb52cf2
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-10-27 21:02:07 +0530

    Fixed Source update

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch corvus_RMX1941-userdebug
+export USE_DEXOPT=true
 export TZ=Asia/Kolkata #put before last build command
 make corvus
 
