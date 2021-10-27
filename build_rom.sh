934ba12d89a702bdbd6c84578077ad715515d573
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-27 19:28:38 +0100

    vince: switch source to AEX.

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rk134/local_manifests.git --depth 1 -b weeb .repo/local_manifests
+git clone https://github.com/rk134/local_manifests.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch weeb_vince-eng
+lunch aosp_vince-eng
 export TZ=Asia/Dhaka #put before last build command
-make weeb-prod
-
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
