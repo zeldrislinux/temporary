462f5040631966391959c2c58e047efa76d7dee6
Razs <73821326+rasenss@users.noreply.github.com>
2021-10-29 10:43:02 +0700

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_ginkgo-userdebug
+export PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 export WITH_GAPPS=true
 export TZ=Asia/Daka #put before last build command
 m aex
