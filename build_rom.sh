6f707085d94656ecb4ec7c885090ee21571e3e7e
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-10-31 17:35:26 +0530

    __

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_pine-user
 export PRODUCT_BROKEN_VERIFY_USES_LIBRARIES=true
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
