d5c22311ac16db41355328d1e564600e626c9cf1
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-10-31 10:49:28 +0530

    Build user type

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ source build/envsetup.sh
 lunch aosp_pine-user
 export PRODUCT_BROKEN_VERIFY_USES_LIBRARIES=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
