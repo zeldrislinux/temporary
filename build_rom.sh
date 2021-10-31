6ba519362910255eb8ad8b5f3b339e85c764932f
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-10-31 14:06:36 +0530

    Changed Hal's and fixed all errors

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,11 @@
 # sync rom
-export git config --global user.name ASHISH11948
-export git config --global user.email jaswalashish48@gmail.com
 repo init --depth=1 --no-repo-verify -u https://github.com/LessAosp/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ASHISH11948/local_manifest.git --depth 1 -b ashish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_olivewood-userdebug
+lunch aosp_olives-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
