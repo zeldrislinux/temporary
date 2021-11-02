3dcb44b7ca64c8990cbac0b39218c4a28b3fb78f
PrajjuS <theprajjus@gmail.com>
2021-11-02 20:59:03 +0530

    vince-ProjectSakura-PrajjuS: Change local manifest branch
    Change-Id: Id3856ac80a72eafa499f33367a0cc94560022ebe
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/PrajjuS/local_manifest_vince.git --depth 1 -b sakura .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectSakura/android.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/PrajjuS/local_manifest_vince.git --depth 1 -b sakura-backup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
