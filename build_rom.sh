46238f9e93b6996e0c012c0e66701d7a1df3f084
PrajjuS <theprajjus@gmail.com>
2021-11-02 13:50:12 +0530

    vince-ProjectSakura-PrajjuS: Build with gapps
    Change-Id: Ib36db083c623c79f3a8ee618576b37a37b6770f2
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_vince-user
+export SAKURA_BUILD_TYPE=basicgapps
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
