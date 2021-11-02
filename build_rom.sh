1c700314bbe0d9d750756af5e3825db9a7bd83a4
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-11-02 16:21:35 +0700

    Forgot to change

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch derp_vayu-eng
+lunch aosp_RMX2001-eng
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
