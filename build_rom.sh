a60369e61c02103ddda8e2ee7f482ff9e1b95b96
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2021-11-04 16:58:00 +0600

    Initial

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b desc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
