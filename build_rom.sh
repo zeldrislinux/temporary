d22724c14081d8908fbef141a4a1fd40771b7162
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-11-07 14:45:33 +0530

    first pxyos Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixysOS/manifest.git -b Twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixysOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b pxy-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
