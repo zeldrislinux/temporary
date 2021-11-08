2faa3ed0902a4648b35eea3b1b1dcf3f85d80f0c
PrajjuS <theprajjus@gmail.com>
2021-11-08 14:54:38 +0530

    vince-ProjectSakura-PrajjuS: build vanilla
    Change-Id: If5363e14a5628ccb518dc076e028ebf5da6c6eff
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_vince-user
-export SAKURA_BUILD_TYPE=basicgapps
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
