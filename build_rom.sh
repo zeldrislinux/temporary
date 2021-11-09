7d63f1fb113cc4464d6ec8fc1a6123177e0fb4ef
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-11-09 14:53:03 +0700

    NgabLu detected

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-komodo_X00TD-userdebug
+lunch komodo_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 masak komodo
 
