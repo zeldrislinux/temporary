0a350eb630e30415ffdbd26fa5364fe1f11565b7
Aditya Singh <adityasingh1768@gmail.com>
2021-11-09 14:04:42 +0530

    Include gcam

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_r2p-userdebug
+lunch aosp_r2p-user
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
