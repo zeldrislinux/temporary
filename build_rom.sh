c7d2edc7c86382c99550a478546171f856a0766b
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-11-11 16:37:40 +0530

    Build elixir

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-rm -rf hardware/qcom-caf/msm8996/display
 lunch aosp_pine-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
