d3862ea5270c5986f84cf37002d00382b0eaf1e1
Yusuf Abidin <yusuff8621@gmail.com>
2021-11-14 05:34:24 +0700

    platina: build: arrow gapps

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch arrow_platina-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export ARROW_GAPPS=true
 export SKIP_API_CHECK=true
 export SKIP_ABI_CHECK=true
 export TZ=Asia/Jakarta #put before last build command
