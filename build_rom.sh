e58c03924fc49a9ac75c56e30575eacf2c13df2d
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-11-13 20:21:52 +0530

    Added fps info  sepolicys

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_sweet-user
+lunch corvus_sweet-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka corvus
 
