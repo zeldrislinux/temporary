7eb2048843bd6cb5af5f99cadf7be5ebda718599
Xisan <ziauddinzisan2001@gmail.com>
2021-11-13 11:38:29 +0600

    old is gold :3

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/pixeldust-project-caf/manifest -b quiche -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/pixeldust-project-caf/manifest -b quiche -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Xeesan/local_manifest.git --depth 1 -b pd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
