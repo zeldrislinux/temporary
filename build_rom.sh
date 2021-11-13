f0dba2fa8977c95131bd98e80b66094fdcce7f07
Xisan <ziauddinzisan2001@gmail.com>
2021-11-14 01:41:07 +0600

    Trying Again

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch pixeldust_beryllium-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 make pixeldust
 
