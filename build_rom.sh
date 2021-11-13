5646936c9afa192aa16875953530dac5bad91997
Naveen <naveen4264nk@gmail.com>
2021-11-13 20:33:07 +0530

    Typo in name

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/N4veeNK/local-manifest.git --depth 1 -b fusion .repo/local_manifests
+git clone https://github.com/N4veenNK/local-manifest.git --depth 1 -b fusion .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
