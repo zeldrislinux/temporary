d4af1f5f870da24d087d37889febd4cad9535623
WalkingDead <lexaxx14@gmail.com>
2021-11-14 08:50:56 +0400

    citrus: initial build RR

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/deadline646/local_manifest.git --depth 1 -b RR .repo/local_manifests
+git clone https://github.com/WalkingDead3/manifest.git --depth 1 -b RR .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #  lbuild rom
 source build/envsetup.sh
 lunch rr_citrus-userdebug
-export KBUILD_BUILD_USER="deadline646"; export KBUILD_BUILD_HOST="root"
+export KBUILD_BUILD_USER="WalkingDead3"
+export KBUILD_BUILD_HOST="WalkingCI"
 export TZ=Asia/Kolkata
 make bacon
 
