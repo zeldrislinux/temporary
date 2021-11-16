e504fefd2151cd049ce9c820ffabe66dbd9a1c74
Zacky Marzuki <33113347+optimax-di@users.noreply.github.com>
2021-11-16 12:44:11 +0700

    Switch to Bengal

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u ghttps://github.com/PixelExperience/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/optimax-di/Local-Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
