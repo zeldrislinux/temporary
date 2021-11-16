d1ad392789fc394fd34b9869ef88734dd8740422
WalkingDead <lexaxx14@gmail.com>
2021-11-16 07:07:19 +0400

    citrus: rebuild with face unlock

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WalkingDead3/manifest.git --depth 1 -b RR .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #  lbuild rom
 source build/envsetup.sh
 lunch rr_citrus-userdebug
-export KBUILD_BUILD_USER="WalkingDead3"
-export KBUILD_BUILD_HOST="WalkingCI"
-export TZ=Asia/Kolkata
-make bacon
+export TZ=Europe/Samara
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
