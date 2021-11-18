83f176a050d19d5974bf7a079604707b48040b9c
P-Salik <peersalik16@gmail.com>
2021-11-18 15:26:08 +0530

    LineageOS 19.0 initial OSS build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/P-Salik/local_manifest --depth 1 -b LineageOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
