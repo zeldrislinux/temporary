ff216162a910c50627e75906855f8337f34d9707
WalkingDead <lexaxx14@gmail.com>
2021-11-19 13:08:51 +0400

    juice: update LOS 18.1

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/WalkingProjekt-juice/manifest --depth 1 -b los-18 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build ro
+# build rom
 source build/envsetup.sh
 lunch lineage_juice-userdebug
 export TZ=Europe/Samara
