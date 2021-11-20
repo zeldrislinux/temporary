b4c32165711a9dab391ddb9ee6f7b92e8886c6df
Razs <73821326+rasenss@users.noreply.github.com>
2021-11-20 23:19:55 +0700

    Use neverallows

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch voltage_ginkgo-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/jawa #put before last build command
 brunch ginkgo
 
