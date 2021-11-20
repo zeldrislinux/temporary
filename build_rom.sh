1967e2a6e4478910ecba2f01581033e480ad62df
WalkingDead <lexaxx14@gmail.com>
2021-11-20 09:46:03 +0300

    juice: inital build HentaiOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # Sync ROM
-repo init --depth=1 --no-repo-verify -u git://github.com/hentaiOS/platform_manifest.git -b Rika -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/hentaiOS/platform_manifest.git -b Rika -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WalkingProjekt-juice/manifest --depth 1 -b Rika .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
