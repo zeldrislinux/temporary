36ab47f3f3c496a01dc8074fffc0fb9390493f3a
Eko Rudianto <82219556+xoo2001@users.noreply.github.com>
2021-11-21 14:19:13 +0700

    fixed

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b nad .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 source build/envsetup.sh
 lunch nad_ido-userdebug
