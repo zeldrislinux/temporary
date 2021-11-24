657d493ae276d9c2fd4c9e9010e43cb3000a7d82
Xisan <ziauddinzisan2001@gmail.com>
2021-11-24 18:16:19 +0600

    Syberia: Changed Hals

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 10.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Xeesan/local_manifest.git --depth 1 -b syb-q .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Xeesan/local_manifest.git --depth 1 -b syb-test .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
