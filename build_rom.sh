6ca15e78fea104059a4f9ba188184705dfc13ef1
Zacky Marzuki <33113347+optimax-di@users.noreply.github.com>
2021-11-24 17:04:45 +0700

    Update 5.2.1

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch dot_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Jakarta
+export TZ=Asia/Jakarta #PB
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
