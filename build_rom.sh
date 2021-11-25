532f539417d5f4c5f6d08c86a12691ab5e7e4b30
Eko Rudianto <82219556+xoo2001@users.noreply.github.com>
2021-11-26 06:47:22 +0700

    fixed

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ArcaneOS/Arcane_manifes
 git clone https://github.com/Infinix-MT6768-Series/local_manifest.git --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_hot10-userdebug
 export TZ=Asia/Jakarta
