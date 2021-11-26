e5d0bbcee0991565b36add6cabdd7d187dc5132b
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-27 06:27:46 +0700

    Fix Maybe

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lighthouse_mido-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka lighthouse
 
