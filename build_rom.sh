58cc33de65f4d6c6b0b0ba02ac277f51acbd075f
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-27 06:40:25 +0700

    fix

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch lighthouse_mido-userdebug
+lunch lighthouse_mido-user
+export SKIP_ABI_CHECK=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
