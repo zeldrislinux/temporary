93affe76524b67f6a72a1c82f858ff91eb63772a
Kneba <abenkenari@gmail.com>
2021-11-29 20:38:01 +0700

    maybe fixed

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch statix_X00TD-userdebug
+export SKIP_ABI_CHECKS=true
 export BUILD_USERNAME=tom
 export BUILD_HOSTNAME=prjkt
 export TZ=Asia/Dhaka #put before last build command
