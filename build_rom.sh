112e3cf4b33730c20ec904f226acc08043241e66
Kneba <abenkenari@gmail.com>
2021-11-30 00:00:32 +0700

    dizzy build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch statix_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_USERNAME=tom
 export BUILD_HOSTNAME=prjkt
 export TZ=Asia/Dhaka #put before last build command
