fa224ade5a3fc3eb1edf52b6e856c8b0f2ba0422
Zacky Marzuki <33113347+optimax-di@users.noreply.github.com>
2021-11-30 08:32:23 +0700

    DotOs Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch dot_juice-userdebug
-export WITH_GAPPS=true #build using gapps
+export WITH_GAPPS=true
 export TZ=Asia/Jakarta
 make bacon
 
