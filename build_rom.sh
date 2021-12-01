1874041e4166002a954f66a36cc506127cf57cda
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-12-01 20:13:55 +0700

    fix ABI

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch syberia_mido-userdebug
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
