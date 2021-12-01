57e38ff725822bd8fa8efb6e726e2d92d0f09fc2
Zacky Marzuki <33113347+optimax-di@users.noreply.github.com>
2021-12-01 08:46:03 +0700

    MengDot

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch dot_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export WITH_GAPPS=true #Remove if build vanilla
+export WITH_GAPPS=true
 export TZ=Asia/Jakarta
 make bacon
 
