72fbc5173399e6ed23d88e2d864667fa55d52702
Kneba <abenkenari@gmail.com>
2021-12-03 22:25:40 +0700

    Top global error

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch rr_X00TD-userdebug
 export BUILD_USERNAME=venom
 export BUILD_HOSTNAME=cirrus
-export TZ=Asia/Japan #put before last build command
+export TZ=Asia/Tokyo #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
