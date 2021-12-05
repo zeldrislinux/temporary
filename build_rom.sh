20f017936789d791f793463e3026a51313bb8f2a
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-05 14:23:55 +0700

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch voltage_ginkgo-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 brunch ginkgo
 
