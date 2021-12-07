bb94643a227f735587c15958fa8b6ec27b40989f
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-12-07 13:49:45 +0530

    vayu: posp: 4

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/PotatoProject/manifest 
 git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_posp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch potato_vayu-userdebug
 export TZ=Asia/Kolkata #put before last build command
