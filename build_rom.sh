a55d28a050912e111d3b072282807d5a3fddb214
nhAsif <najmulhasan3609@gmail.com>
2021-12-08 14:14:18 +0000

    fix error

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,6 @@
 # sync rom
-<<<<<<< HEAD
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-=======
 repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b losfe .repo/local_manifests
->>>>>>> 658d865268ef74d32295dbc70451e5b917fed2e7
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
