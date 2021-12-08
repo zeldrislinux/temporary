658d865268ef74d32295dbc70451e5b917fed2e7
Asif <49162687+nhAsif@users.noreply.github.com>
2021-12-08 13:02:43 +0600

    Update dt & kernel

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b losfe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_rosy-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka
 make bacon 
 
 
