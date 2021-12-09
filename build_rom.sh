86bb2edfe7f0a7011dd5b5d7e4e023c49ecdeb2b
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-09 18:41:48 +0700

    Fix manifest error

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export WITH_GAPPS=true
 lunch syberia_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
