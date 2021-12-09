d1277419609ab316405f35757137b67c702242f6
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-10 05:38:37 +0700

    Try gapps build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
 lunch syberia_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
