f09e7943ceddc083979e703da96e15b4c8676c16
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2021-12-10 10:00:09 +0530

    Initial Project Awaken.

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault # Initilizeing Pixel Experiance sources
-git clone https://github.com/RahulPalXDA/local_manifest.git --depth=1 -b pex_tulip .repo/local_manifests # adding builder's local_manifest to Initilized sources
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault # Initilizeing Pixel Experiance sources
+git clone https://github.com/RahulPalXDA/local_manifest.git --depth=1 -b awaken_tulip .repo/local_manifests # adding builder's local_manifest to Initilized sources
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 # Syncing previously Initilized sources
 
 # build rom
 source build/envsetup.sh
-lunch aosp_tulip-userdebug
+lunch awaken_tulip-userdebug
+export USE_GAPPS := false
 export TZ=Asia/Kolkata #put before last build command (setting timezone)
 mka bacon
 
