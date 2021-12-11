7017d3fe294456c3c29f3c11bd15e44ca0776db2
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-11 22:59:48 +0700

    Change clang

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch voltage_ginkgo-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta #put before build command 
 brunch ginkgo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
