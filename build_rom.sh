673532c927c30c54cbe34248745ade1853de2941
Enn's <65471980+Enprytna@users.noreply.github.com>
2021-12-13 17:13:44 +0700

    Fix sempolicy

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch xperience_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
+export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_USERNAME=endi
 export BUILD_HOSTNAME=enprytna
 make bacon
