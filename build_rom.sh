d9f46d29b5150d33bf706bd34a0e820ba18d829d
xoo2001 <ekora2001@gmail.com>
2021-12-14 23:13:55 +0000

    intial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://git.stag-os.org/StagOS/manifest.git -b r11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-LegionOS/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Infinix-MT6768-Series/local_manifest.git --depth 1 -b univ .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch stag_hot10-userdebug
+lunch legion_hot10-userdebug
 export TZ=Asia/Jakarta
-make stag
+make legion
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
