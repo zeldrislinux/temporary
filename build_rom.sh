db002a8903af81009e025fa18bdaacdcd17f45cf
Kneba <abenkenary3@gmail.com>
2021-12-16 16:39:35 +0700

    Test Fb changed

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch amy_X00TD-userdebug
 export BUILD_USERNAME=venom
 export BUILD_HOSTNAME=carnage
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Bangkok #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
