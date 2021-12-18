74bf401262cb34fe5d1d13af38c739beb2fde1f9
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2021-12-18 12:45:50 +0700

    test fix error

--- build_rom.sh
+++ build_rom.sh
@@ -11,6 +11,7 @@ export BUILD_USER=android-build
 export BUILD_HOST=GeForce-RTX
 export BUILD_USERNAME=GeForce-RTX
 export BUILD_HOSTNAME=android_build
+export BUILD_BROKEN_DUP_RULES := true
 make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
