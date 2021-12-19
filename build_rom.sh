f79b6efd761ed5c1c203542e0be9555747011b43
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2021-12-19 17:32:06 +0700

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WallBreaker2407/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
