545e7c53c16f688b695679204dd1f2a2004d7ea6
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-19 10:22:32 +0700

    Initial

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom.
-repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b fire -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rasenss/manifest_local.git --depth 1 -b Aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_ginkgo-user
+lunch aosp_ginkgo-user
 export WITH_GAPPS=true
 export TZ=Asia/Jakarta #put before last build comman
-mka spark
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line).
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
