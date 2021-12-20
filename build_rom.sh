e41dc3dcf31290de1f6b2e629fc9d04b5907ca4c
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-12-20 10:59:35 +0700

    BTLP

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
-export BUILD_USER=free
-export BUILD_HOST=seber
+export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Bangkok #put before last build command
 make xd
 
