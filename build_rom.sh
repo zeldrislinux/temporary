4cdb15c3d892041586e6a0ab5d475fabb3166f2f
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-21 07:44:01 +0700

    vanilla build for now

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_ginkgo-user
-export WITH_GAPPS=true
 export TZ=Asia/Jakarta #put before last build 
 make bacon
 
