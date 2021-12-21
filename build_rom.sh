dd4efff80ad4d022c4ffd0a4ebf4824a6ef303f2
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-21 14:27:02 +0700

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export WITH_GAPPS=true
 lunch aosp_ginkgo-user
 export TZ=Asia/Jakarta #put before last build 
 make bacon
