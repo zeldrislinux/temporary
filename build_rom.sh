79ed388127f9b342f5c57ab3d71298d5420e57a9
sanjeevstunner <sanjeevstunner1@gmail.com>
2021-12-21 20:47:27 +0530

    twolip: PE+: Pie: initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b twolip_aosp-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b pie-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b twolip_aosp-9 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
