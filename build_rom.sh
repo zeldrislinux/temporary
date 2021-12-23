b6abea9f4e8af09f0db5411deab258f558ca4c05
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-23 07:56:29 +0700

    Try with not neverallows

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export WITH_GAPPS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 lunch aosp_ginkgo-userdebug
-export TZ=Asia/Jakarta #put before last build 
+export TZ=Asia/Jakarta #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line).
