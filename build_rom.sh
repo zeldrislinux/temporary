e6698580239b45f2b3e256d524ffaf0414c1d6fd
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-24 05:00:17 +0700

    Vanilla build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch aosp_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build comman
