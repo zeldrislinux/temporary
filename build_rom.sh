29d0047bae7f18fdf7576b17bce84a0b7d2d7dc5
Kitsunejasutin <73803767+Kitsunejasutin@users.noreply.github.com>
2021-12-25 14:14:52 +0800

    Update repo init line

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11 --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11 --depth=1 and -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kitsunejasutin/local_manifest.git --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
