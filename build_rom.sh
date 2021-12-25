2b3f39b3698c16be02fe028060260a47fdd46688
Razs <73821326+rasenss@users.noreply.github.com>
2021-12-25 11:18:12 +0700

    Fix typo on flags

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_ginkgo-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=tre
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last command
 mka bacon
 
