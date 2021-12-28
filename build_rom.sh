ff91cbfe3e9dee93c1792158ead4d1597d4ed7ac
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2021-12-28 16:56:55 +0700

    Include Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync rom1
 repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WallBreaker2407/local_manifest.git --depth 1 -b Havoc-OS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom1
 source build/envsetup.sh
 lunch havoc_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
