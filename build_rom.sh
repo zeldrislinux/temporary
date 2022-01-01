d237cdcc1dfc7b62abdc1bf0f83b72fdbe990bff
Razs <73821326+rasenss@users.noreply.github.com>
2022-01-01 10:14:23 +0700

    Initial Pex

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest.git -b snow  -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b pex-12 .repo/local_manifests
+git clone https://github.com/rasenss/manifest_local --depth 1 -b Pixel .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
