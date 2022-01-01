8fbb2c8c6d6d8955b9bc43a1f988f224e27d4572
WalkingDead <lexaxx14@gmail.com>
2022-01-01 10:27:33 +0400

    citrus: inital build LOS 18.1

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_citrus-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Europe/Samara #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
