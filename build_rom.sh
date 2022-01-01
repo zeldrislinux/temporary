571e7567edc67465a8919c4eed1dd083adaa2e8f
STRK-ND <raj15400881@gmail.com>
2022-01-02 01:58:28 +0530

    POSp A12

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
 git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
 # build rom
-. build/envsetup.sh
-lunch corvus_X00TD-user
-export ALLOW_MISSING_DEPENDENCIES=true
-export PRODUCT_BROKEN_VERIFY_USES_LIBRARIES=true
-export RELAX_USES_LIBRARY_CHECK=true
-export TZ=Asia/Dhaka #put before last build command
-make corvus
+source build/envsetup.sh
+lunch potato_X00TD-userdebug
+export TZ=Asia/Kolkata #put before last build command
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
