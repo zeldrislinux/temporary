94fa7d547c1e2e3a00382b11fa2db19dfcde7047
Amy <54113654+00p513-dev@users.noreply.github.com>
2022-01-04 19:07:36 +0000

    use curl instead of the function

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,3 @@
-transfer(){ if [ $# -eq 0 ];then echo "No arguments specified.\nUsage:\n  transfer <file|directory>\n  ... | transfer <file_name>">&2;return 1;fi;if tty -s;then file="$1";file_name=$(basename "$file");if [ ! -e "$file" ];then echo "$file: No such file or directory">&2;return 1;fi;if [ -d "$file" ];then file_name="$file_name.zip" ,;(cd "$file"&&zip -r -q - .)|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null,;else cat "$file"|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;else file_name=$1;curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;}
-
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest -b arcadia -g default,-mips,-darwin,-notdefault
 git clone https://github.com/00p513-dev/local_manifests -b bliss-arcadia-sargo --depth 1 .repo/local_manifests
@@ -17,5 +15,5 @@ m blissify
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
 for file in out/target/product/*/Bliss*.zip*
-  transfer "$file"
+  curl --upload-file "$file" https://transfer.sh/$file
 done
