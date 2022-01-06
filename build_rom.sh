a85e5ef1f6617814bd869212ce603ac9981d81cc
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2022-01-06 13:38:00 +0530

    initial elixir build for holland2

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RahulPalXDA/manifest.git --depth 1 -b elixir .repo/local_manifests
+git clone https://github.com/RahulPalXDA/local_manifest.git --depth 1 -b holland2_elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_tulip-userdebug
+lunch aosp_holland2-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
