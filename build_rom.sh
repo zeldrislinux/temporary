7a6c3885c1c855b63b235ba73108c56db554666f
Razs <73821326+rasenss@users.noreply.github.com>
2022-01-11 21:38:25 +0700

    Try fix wfd

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom.
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rasenss/manifest_local.git --depth 1 -b Aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -8,7 +8,7 @@ source build/envsetup.sh
 export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch aosp_ginkgo-userdebug
-export TZ=Asia/Jakarta #put before last build comman
+export TZ=Asia/Jakarta #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line).
