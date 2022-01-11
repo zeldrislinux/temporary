9117e87337f4e2b2035b3ec09f08a6c3b85cae5e
Kneba <abenkenary3@gmail.com>
2022-01-11 11:01:59 +0700

    update manifest

--- build_rom.sh
+++ build_rom.sh
@@ -7,9 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch potato_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export PRODUCT_BROKEN_VERIFY_USES_LIBRARIES=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Bangkok #put before last build command
 brunch X00TD
 
