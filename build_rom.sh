683c191fcdae0abe2bfe726f749ed08555b12b68
Kneba <abenkenary3@gmail.com>
2022-01-12 13:56:41 +0700

    update deps [2]

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
 # sync roms
-repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kneba/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 source build/envsetup.sh
-lunch potato_X00TD-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
+export PRODUCT_BROKEN_VERIFY_USES_LIBRARIES=true
+export RELAX_USES_LIBRARY_CHECK=true
 export SELINUX_IGNORE_NEVERALLOWS=true
+lunch potato_X00TD-userdebug
 export TZ=Asia/Bangkok #put before last build command
 brunch X00TD
 
