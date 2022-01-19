7075cfef5081432e79210c9c95af123488107291
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-01-19 17:01:16 +0700

    TEST BUILD

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 11.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/WallBreaker2407/local_manifest.git --depth 1 -b aex .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_X00TD-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Jakarta #put before last build command
+export BUILD_BROKEN_DUP_RULES := true
+export SELINUX_IGNORE_NEVERALLOWS := true
+export WITH_GAPPS= true
+export BUILD_USERNAME=GeForce-RTX
+export BUILD_HOSTNAME=android_build
 m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
